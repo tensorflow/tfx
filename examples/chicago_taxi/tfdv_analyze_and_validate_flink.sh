7eb201e1c51d07ec6deffd976f565d76651e1631
Ankur Goenka <ankurgoenka@gmail.com>
2019-02-13 16:35:20 -0800

    Remove debug logs

--- examples/chicago_taxi/tfdv_analyze_and_validate_flink.sh
+++ examples/chicago_taxi/tfdv_analyze_and_validate_flink.sh
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 set -u
-set -x
 
 echo Starting distributed TFDV stats computation and schema generation...
 
