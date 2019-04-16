3ef6081dd3d614439fb4e0767af2fb2d8afedb58
Tamera Lanham <tamera.lanham@gmail.com>
2019-04-16 14:59:27 -0400

    feat: parquet example gen executor

--- tfx/components/example_gen/parquet_example_gen/executor.py
+++ tfx/components/example_gen/parquet_example_gen/executor.py
@@ -13,7 +13,62 @@
 # limitations under the License.
 """Generic TFX parquet example gen executor."""
 
+import apache_beam as beam
+import tensorflow as tf
 from tfx.components.example_gen import base_example_gen_executor
+from tfx.utils import io_utils
+from tfx.utils import types
+
+def _dict_to_example(instance):
+  """Decoded parquet to tf example."""
+  # Ideally this would use feature types from the parquet schema, but that isn't
+  # readily available from beam
+  feature = {}
+  for key, value in instance.items():
+    if value is None:
+      feature[key] = tf.train.Feature()
+    elif isinstance(value, int):
+      feature[key] = tf.train.Feature(
+          int64_list=tf.train.Int64List(value=[value]))
+    elif isinstance(value, float):
+      feature[key] = tf.train.Feature(
+          float_list=tf.train.FloatList(value=[value]))
+    else:
+      feature[key] = tf.train.Feature(
+          bytes_list=tf.train.BytesList(value=[value.encode('utf-8')]))
+  return tf.train.Example(features=tf.train.Features(feature=feature))
+
+@beam.ptransform_fn
+@beam.typehints.with_input_types(beam.Pipeline)
+@beam.typehints.with_output_types(tf.train.Example)
+def _ParquetToExample(
+    pipeline, input_dict,
+    exec_properties):
+  """Read CSV file and transform to TF examples.
+
+  Args:
+    pipeline: beam pipeline.
+    input_dict: Input dict from input key to a list of Artifacts.
+      - input-base: input dir that contains csv data. csv files must have header
+        line.
+    exec_properties: A dict of execution properties.
+
+  Returns:
+    PCollection of TF examples.
+  """
+  input_base = types.get_single_instance(input_dict['input-base'])
+  base_uri = input_base.uri
+  tf.logging.info('Processing input parquet data {} to TFExample.'.format(base_uri))
+
+  return (pipeline
+         | 'ReadFromParquet' >> beam.io.ReadFromParquet(base_uri)
+         | 'ToTFExample' >> beam.Map(_dict_to_example))
+
+
 
 class Executor(base_example_gen_executor.BaseExampleGenExecutor):
   """Generic TFX parquet example gen executor."""
+
+  def GetInputSourceToExamplePTransform(self):
+    """Returns PTransform for parquet to TF examples."""
+    return _ParquetToExample
