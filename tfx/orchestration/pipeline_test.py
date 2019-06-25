2f5d51ec8a4dd1adba07d88ddf3c569c02eff343
tfx-team <tensorflow-extended-team@google.com>
2019-06-24 18:42:22 -0700

    Include pipeline info and producer component info for each artifacts in logical pipeline.
    PiperOrigin-RevId: 254881586

--- tfx/orchestration/pipeline_test.py
+++ tfx/orchestration/pipeline_test.py
@@ -30,36 +30,30 @@ from tfx.components.base import base_executor
 from tfx.components.base.base_component import ChannelParameter
 from tfx.orchestration import pipeline
 from tfx.utils import channel
+from tfx.utils import types
 
 
-def _make_fake_component_instance(
-    name: Text,
-    inputs: Dict[Text, channel.Channel],
-    outputs: Dict[Text, channel.Channel]):
+def _make_fake_component_instance(name: Text,
+                                  inputs: Dict[Text, channel.Channel],
+                                  outputs: Dict[Text, channel.Channel]):
 
   class _FakeComponentSpec(base_component.ComponentSpec):
     COMPONENT_NAME = name
     PARAMETERS = {}
-    INPUTS = dict([
-        (arg, ChannelParameter(type_name=channel.type_name))
-        for arg, channel in inputs.items()
-    ])
-    OUTPUTS = dict([
-        (arg, ChannelParameter(type_name=channel.type_name))
-        for arg, channel in outputs.items()
-    ] + [('output', ChannelParameter(type_name=name))])
+    INPUTS = dict([(arg, ChannelParameter(type_name=channel.type_name))
+                   for arg, channel in inputs.items()])
+    OUTPUTS = dict([(arg, ChannelParameter(type_name=channel.type_name))
+                    for arg, channel in outputs.items()] +
+                   [('output', ChannelParameter(type_name=name))])
 
   class _FakeComponent(base_component.BaseComponent):
 
     SPEC_CLASS = _FakeComponentSpec
     EXECUTOR_CLASS = base_executor.BaseExecutor
 
-    def __init__(self,
-                 name: Text,
-                 spec_kwargs: Dict[Text, Any]):
+    def __init__(self, name: Text, spec_kwargs: Dict[Text, Any]):
       spec = _FakeComponentSpec(
-          output=channel.Channel(type_name=name),
-          **spec_kwargs)
+          output=channel.Channel(type_name=name), **spec_kwargs)
       super(_FakeComponent, self).__init__(spec=spec, name=name)
 
   spec_kwargs = dict(itertools.chain(inputs.items(), outputs.items()))
@@ -71,7 +65,8 @@ class PipelineTest(tf.test.TestCase):
   def setUp(self):
     self._tmp_file = os.path.join(
         os.environ.get('TEST_UNDECLARED_OUTPUTS_DIR', self.get_temp_dir()),
-        self._testMethodName, tempfile.mkstemp(prefix='cli_tmp_')[1])
+        self._testMethodName,
+        tempfile.mkstemp(prefix='cli_tmp_')[1])
     self._original_tmp_value = os.environ.get(
         'TFX_JSON_EXPORT_PIPELINE_ARGS_PATH', '')
 
@@ -109,11 +104,12 @@ class PipelineTest(tf.test.TestCase):
     self.assertItemsEqual(my_pipeline.components[0].downstream_nodes,
                           [component_b, component_c, component_e])
     self.assertEqual(my_pipeline.components[-1], component_e)
-    self.assertDictEqual(my_pipeline.pipeline_args, {
-        'pipeline_name': 'a',
-        'pipeline_root': 'b',
-        'log_root': 'c'
-    })
+    self.assertDictEqual(
+        my_pipeline.pipeline_args, {
+            'pipeline_name': 'a',
+            'pipeline_root': 'b',
+            'log_root': 'c',
+        })
 
   def test_pipeline_with_loop(self):
     channel_one = channel.Channel(type_name='channel_one')
@@ -126,27 +122,21 @@ class PipelineTest(tf.test.TestCase):
             'a': component_a.outputs.output,
             'one': channel_one
         },
-        outputs={
-            'two': channel_two
-        })
+        outputs={'two': channel_two})
     component_c = _make_fake_component_instance(
         name='component_b',
         inputs={
             'a': component_a.outputs.output,
             'two': channel_two
         },
-        outputs={
-            'three': channel_three
-        })
+        outputs={'three': channel_three})
     component_d = _make_fake_component_instance(
         name='component_b',
         inputs={
             'a': component_a.outputs.output,
             'three': channel_three
         },
-        outputs={
-            'one': channel_one
-        })
+        outputs={'one': channel_one})
 
     with self.assertRaises(RuntimeError):
       pipeline.Pipeline(
@@ -154,6 +144,42 @@ class PipelineTest(tf.test.TestCase):
           pipeline_root='b',
           log_root='c',
           components=[component_c, component_d, component_b, component_a])
+
+  def test_pipeline_with_artifact_info(self):
+    artifacts_collection = [types.TfxArtifact('channel_one')]
+    channel_one = channel.Channel(
+        type_name='channel_one',
+        artifacts=artifacts_collection)
+    component_a = _make_fake_component_instance(
+        name='component_a',
+        inputs={},
+        outputs={'one': channel_one})
+    component_b = _make_fake_component_instance(
+        name='component_b',
+        inputs={
+            'a': component_a.outputs.one,
+        },
+        outputs={})
+
+    my_pipeline = pipeline.Pipeline(
+        pipeline_name='a',
+        pipeline_root='b',
+        log_root='c',
+        components=[component_b, component_a])
+    expected_artifact = types.TfxArtifact('channel_one')
+    expected_artifact.name = 'one'
+    expected_artifact.pipeline_name = 'a'
+    expected_artifact.pipeline_timestamp_ms = 0
+    expected_artifact.producer_component = 'component_a'
+    self.assertItemsEqual(my_pipeline.components, [component_a, component_b])
+    self.assertEqual(component_a.outputs.one._artifacts[0].pipeline_name, 'a')
+    self.assertEqual(component_a.outputs.one._artifacts[0].producer_component,
+                     component_a.component_id)
+    self.assertEqual(component_a.outputs.one._artifacts[0].name, 'one')
+    self.assertEqual(component_b.inputs.a._artifacts[0].pipeline_name, 'a')
+    self.assertEqual(component_b.inputs.a._artifacts[0].producer_component,
+                     component_a.component_id)
+    self.assertEqual(component_b.inputs.a._artifacts[0].name, 'one')
 
   def test_pipeline_decorator(self):
 
@@ -168,11 +194,12 @@ class PipelineTest(tf.test.TestCase):
 
     self.assertItemsEqual(my_pipeline.components,
                           [self.component_a, self.component_b])
-    self.assertDictEqual(my_pipeline.pipeline_args, {
-        'pipeline_name': 'a',
-        'pipeline_root': 'b',
-        'log_root': 'c'
-    })
+    self.assertDictEqual(
+        my_pipeline.pipeline_args, {
+            'pipeline_name': 'a',
+            'pipeline_root': 'b',
+            'log_root': 'c',
+        })
 
   def test_pipeline_save_pipeline_args(self):
     os.environ['TFX_JSON_EXPORT_PIPELINE_ARGS_PATH'] = self._tmp_file
