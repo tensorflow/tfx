516228a79bf173905649e3729fad2a0cbfffaae7
bumbleblo <bumbleblo2013@gmail.com>
2019-07-14 21:51:47 -0300

    #51 - Setting airflow database volume
    Signed-off-by: bumbleblo <bumbleblo2013@gmail.com>

--- tfx/examples/docker_tutorial/setup/setup.sh
+++ tfx/examples/docker_tutorial/setup/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+airflow initdb &
+rm /home/airflow/airflow-webserver.pid &
 airflow webserver -p 8080  &
 airflow scheduler  &
-jupyter notebook --ip 0.0.0.0 --port 8000 --allow-root
+jupyter notebook --ip 0.0.0.0 --port 8000 --allow-root --NotebookApp.token=""
