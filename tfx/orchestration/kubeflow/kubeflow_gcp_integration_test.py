8debf1969b67342a64d7a24cfade7de37b0ecaf9
muchida <muchida@google.com>
2019-08-29 09:35:49 -0700

    Add Kubeflow-AI Platform Pusher integration test
    PiperOrigin-RevId: 266160183

--- tfx/orchestration/kubeflow/kubeflow_gcp_integration_test.py
+++ tfx/orchestration/kubeflow/kubeflow_gcp_integration_test.py
@@ -28,12 +28,15 @@ import tensorflow as tf
 from typing import List, Text
 
 from ml_metadata.proto import metadata_store_pb2
+from tfx.components.base import executor_spec
 from tfx.components.evaluator.component import Evaluator
 from tfx.components.example_gen.csv_example_gen.component import CsvExampleGen
 from tfx.components.model_validator.component import ModelValidator
+from tfx.components.pusher.component import Pusher
 from tfx.components.statistics_gen.component import StatisticsGen
 from tfx.components.trainer.component import Trainer
 from tfx.components.transform.component import Transform
+from tfx.extensions.google_cloud_ai_platform.pusher import executor as ai_platform_pusher_executor
 from tfx.extensions.google_cloud_ai_platform.trainer import executor as ai_platform_trainer_executor
 from tfx.orchestration import data_types
 from tfx.orchestration import metadata
@@ -131,6 +134,35 @@ class KubeflowGCPIntegrationTest(test_utils.BaseKubeflowTest):
 
     return channel_utils.as_channel(published_artifacts[output_key])
 
+  def _delete_ai_platform_model(self, model_name):
+    """Delete pushed model in AI Platform."""
+    # In order to delete model, all versions in the model must be deleted first.
+    versions_command = [
+        'gcloud', 'ai-platform', 'versions', 'list',
+        '--model=%s' % model_name
+    ]
+    versions = subprocess.run(versions_command, stdout=subprocess.PIPE)
+
+    if versions.returncode == 0:
+      tf.logging.info('Model %s has versions %s' % (model_name, versions))
+
+      # First line of the output is the header: [NAME] [DEPLOYMENT_URI] [STATE]
+      for version in versions.stdout.decode('utf-8').strip('\n').split(
+          '\n')[1:]:
+        version = version.split()[0]
+        tf.logging.info('Deleting version %s of model %s' %
+                        (version, model_name))
+        version_delete_command = [
+            'gcloud', '--quiet', 'ai-platform', 'versions', 'delete', version,
+            '--model=%s' % model_name
+        ]
+        subprocess.run(version_delete_command, check=True)
+
+    tf.logging.info('Deleting model %s' % model_name)
+    subprocess.run(
+        ['gcloud', '--quiet', 'ai-platform', 'models', 'delete', model_name],
+        check=True)
+
   def setUp(self):
     super(KubeflowGCPIntegrationTest, self).setUp()
 
@@ -162,26 +194,26 @@ class KubeflowGCPIntegrationTest(test_utils.BaseKubeflowTest):
     schema = standard_artifacts.Schema()
     schema.uri = os.path.join(self._intermediate_data_root,
                               'schema_gen/output/test-pipeline/')
-    self._test_schema = schema
+    self._test_schema = [schema]
 
     # TransformGraph artifact for testing.
     transform_graph = standard_artifacts.TransformGraph()
     transform_graph.uri = os.path.join(
         self._intermediate_data_root,
-        'transform/test-pipeline/transform_output/')
-    self._test_transform_graph = transform_graph
+        'transform/transform_output/test-pipeline/')
+    self._test_transform_graph = [transform_graph]
 
     # Model artifact for testing.
     model = standard_artifacts.Model()
     model.uri = os.path.join(self._intermediate_data_root,
                              'trainer/output/test-pipeline/')
-    self._test_model = model
+    self._test_model = [model]
 
     # ModelBlessing artifact for testing.
     model_blessing = standard_artifacts.ModelBlessing()
     model_blessing.uri = os.path.join(
         self._intermediate_data_root, 'model_validator/blessing/test-pipeline/')
-    self._test_model_blessing = model_blessing
+    self._test_model_blessing = [model_blessing]
 
   def testCsvExampleGenOnDataflowRunner(self):
     """CsvExampleGen-only test pipeline on DataflowRunner invocation."""
@@ -211,7 +243,7 @@ class KubeflowGCPIntegrationTest(test_utils.BaseKubeflowTest):
         Transform(
             input_data=self._input_artifacts(pipeline_name,
                                              self._test_raw_examples),
-            schema=self._input_artifacts(pipeline_name, [self._test_schema]),
+            schema=self._input_artifacts(pipeline_name, self._test_schema),
             module_file=self._taxi_module_file)
     ])
     self._compile_and_run_pipeline(pipeline)
@@ -225,7 +257,7 @@ class KubeflowGCPIntegrationTest(test_utils.BaseKubeflowTest):
             examples=self._input_artifacts(pipeline_name,
                                            self._test_raw_examples),
             model_exports=self._input_artifacts(pipeline_name,
-                                                [self._test_model]),
+                                                self._test_model),
             feature_slicing_spec=evaluator_pb2.FeatureSlicingSpec(specs=[
                 evaluator_pb2.SingleSlicingSpec(
                     column_for_slicing=['trip_start_hour'])
@@ -241,47 +273,62 @@ class KubeflowGCPIntegrationTest(test_utils.BaseKubeflowTest):
         ModelValidator(
             examples=self._input_artifacts(pipeline_name,
                                            self._test_raw_examples),
-            model=self._input_artifacts(pipeline_name, [self._test_model]))
+            model=self._input_artifacts(pipeline_name, self._test_model))
     ])
     self._compile_and_run_pipeline(pipeline)
 
   def testAIPlatformTrainerPipeline(self):
+    """Trainer-only test pipeline on AI Platform Training."""
     pipeline_name = 'kubeflow-aip-trainer-test-{}'.format(self._random_id())
-    # Up-to Transform component
-    components = test_utils.create_e2e_components(
-        self._pipeline_root(pipeline_name), self._data_root,
-        self._taxi_module_file)[:5]
-
-    infer_schema = components[2]
-    transform = components[4]
-
-    trainer = Trainer(
-        executor_class=ai_platform_trainer_executor.Executor,
-        module_file=self._taxi_module_file,
-        transformed_examples=transform.outputs.transformed_examples,
-        schema=infer_schema.outputs.output,
-        transform_output=transform.outputs.transform_output,
-        train_args=trainer_pb2.TrainArgs(num_steps=10000),
-        eval_args=trainer_pb2.EvalArgs(num_steps=5000),
-        custom_config={
-            'ai_platform_training_args': {
-                'project':
-                    self._gcp_project_id,
-                'region':
-                    self._gcp_region,
-                'jobDir':
-                    os.path.join(self._pipeline_root(pipeline_name), 'tmp'),
-                'masterConfig': {
-                    'imageUri': self._container_image,
+    pipeline = self._create_pipeline(pipeline_name, [
+        Trainer(
+            custom_executor_spec=executor_spec.ExecutorClassSpec(
+                ai_platform_trainer_executor.Executor),
+            module_file=self._taxi_module_file,
+            transformed_examples=self._input_artifacts(
+                pipeline_name, self._test_transformed_examples),
+            schema=self._input_artifacts(pipeline_name, self._test_schema),
+            transform_output=self._input_artifacts(pipeline_name,
+                                                   self._test_transform_graph),
+            train_args=trainer_pb2.TrainArgs(num_steps=10000),
+            eval_args=trainer_pb2.EvalArgs(num_steps=5000),
+            custom_config={
+                'ai_platform_training_args': {
+                    'project':
+                        self._gcp_project_id,
+                    'region':
+                        self._gcp_region,
+                    'jobDir':
+                        os.path.join(self._pipeline_root(pipeline_name), 'tmp'),
+                    'masterConfig': {
+                        'imageUri': self._container_image,
+                    }
                 }
-            }
-        })
-    components.append(trainer)
-    pipeline = self._create_pipeline(pipeline_name, components)
-
+            }),
+    ])
     self._compile_and_run_pipeline(pipeline)
 
-  # TODO(muchida): Add test cases for AI Platform Pusher.
+  def testAIPlatformPusherPipeline(self):
+    """Pusher-only test pipeline to AI Platform Prediction."""
+    pipeline_name = 'kubeflow-aip-pusher-test-{}'.format(self._random_id())
+    # AI Platform does not accept '-' in the model name.
+    model_name = ('%s_model' % pipeline_name).replace('-', '_')
+    pipeline = self._create_pipeline(pipeline_name, [
+        Pusher(
+            custom_executor_spec=executor_spec.ExecutorClassSpec(
+                ai_platform_pusher_executor.Executor),
+            model_export=self._input_artifacts(pipeline_name, self._test_model),
+            model_blessing=self._input_artifacts(pipeline_name,
+                                                 self._test_model_blessing),
+            custom_config={
+                'ai_platform_serving_args': {
+                    'model_name': model_name,
+                    'project_id': self._gcp_project_id,
+                }
+            })
+    ])
+    self.addCleanup(self._delete_ai_platform_model, model_name)
+    self._compile_and_run_pipeline(pipeline)
 
 
 if __name__ == '__main__':
