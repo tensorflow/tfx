3193dab4ee050911f72d337103c1d931468258c2
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2019-09-19 13:52:28 -0700

    Update examples to use new format (example_gen.outputs['examples']) instead of old format (example_gen.outputs.examples).
    PiperOrigin-RevId: 270117420

--- tfx/examples/chicago_taxi_pipeline/taxi_pipeline_kubeflow.py
+++ tfx/examples/chicago_taxi_pipeline/taxi_pipeline_kubeflow.py
@@ -150,20 +150,21 @@ def _create_pipeline(
   example_gen = BigQueryExampleGen(query=query)
 
   # Computes statistics over data for visualization and example validation.
-  statistics_gen = StatisticsGen(input_data=example_gen.outputs.examples)
+  statistics_gen = StatisticsGen(input_data=example_gen.outputs['examples'])
 
   # Generates schema based on statistics files.
   infer_schema = SchemaGen(
-      stats=statistics_gen.outputs.output, infer_feature_shape=False)
+      stats=statistics_gen.outputs['output'], infer_feature_shape=False)
 
   # Performs anomaly detection based on statistics and data schema.
   validate_stats = ExampleValidator(
-      stats=statistics_gen.outputs.output, schema=infer_schema.outputs.output)
+      stats=statistics_gen.outputs['output'],
+      schema=infer_schema.outputs['output'])
 
   # Performs transformations and feature engineering in training and serving.
   transform = Transform(
-      input_data=example_gen.outputs.examples,
-      schema=infer_schema.outputs.output,
+      input_data=example_gen.outputs['examples'],
+      schema=infer_schema.outputs['output'],
       module_file=module_file)
 
   # Uses user-provided Python function that implements a model using TF-Learn
@@ -175,9 +176,9 @@ def _create_pipeline(
         custom_executor_spec=executor_spec.ExecutorClassSpec(
             ai_platform_trainer_executor.Executor),
         module_file=module_file,
-        transformed_examples=transform.outputs.transformed_examples,
-        schema=infer_schema.outputs.output,
-        transform_output=transform.outputs.transform_output,
+        transformed_examples=transform.outputs['transformed_examples'],
+        schema=infer_schema.outputs['output'],
+        transform_output=transform.outputs['transform_output'],
         train_args=trainer_pb2.TrainArgs(num_steps=10000),
         eval_args=trainer_pb2.EvalArgs(num_steps=5000),
         custom_config={'ai_platform_training_args': ai_platform_training_args})
@@ -185,17 +186,17 @@ def _create_pipeline(
     # Train using a deprecated flag.
     trainer = Trainer(
         module_file=module_file,
-        transformed_examples=transform.outputs.transformed_examples,
-        schema=infer_schema.outputs.output,
-        transform_output=transform.outputs.transform_output,
+        transformed_examples=transform.outputs['transformed_examples'],
+        schema=infer_schema.outputs['output'],
+        transform_output=transform.outputs['transform_output'],
         train_args=trainer_pb2.TrainArgs(num_steps=10000),
         eval_args=trainer_pb2.EvalArgs(num_steps=5000),
         custom_config={'cmle_training_args': ai_platform_training_args})
 
   # Uses TFMA to compute a evaluation statistics over features of a model.
   model_analyzer = Evaluator(
-      examples=example_gen.outputs.examples,
-      model_exports=trainer.outputs.output,
+      examples=example_gen.outputs['examples'],
+      model_exports=trainer.outputs['output'],
       feature_slicing_spec=evaluator_pb2.FeatureSlicingSpec(specs=[
           evaluator_pb2.SingleSlicingSpec(
               column_for_slicing=['trip_start_hour'])
@@ -203,7 +204,7 @@ def _create_pipeline(
 
   # Performs quality validation of a candidate model (compared to a baseline).
   model_validator = ModelValidator(
-      examples=example_gen.outputs.examples, model=trainer.outputs.output)
+      examples=example_gen.outputs['examples'], model=trainer.outputs['output'])
 
   # Checks whether the model passed the validation steps and pushes the model
   # to a destination if check passed.
@@ -213,14 +214,14 @@ def _create_pipeline(
     pusher = Pusher(
         custom_executor_spec=executor_spec.ExecutorClassSpec(
             ai_platform_pusher_executor.Executor),
-        model_export=trainer.outputs.output,
-        model_blessing=model_validator.outputs.blessing,
+        model_export=trainer.outputs['output'],
+        model_blessing=model_validator.outputs['blessing'],
         custom_config={'ai_platform_serving_args': ai_platform_serving_args})
   except ImportError:
     # Deploy the model on Google Cloud AI Platform, using a deprecated flag.
     pusher = Pusher(
-        model_export=trainer.outputs.output,
-        model_blessing=model_validator.outputs.blessing,
+        model_export=trainer.outputs['output'],
+        model_blessing=model_validator.outputs['blessing'],
         custom_config={'cmle_serving_args': ai_platform_serving_args},
         push_destination=pusher_pb2.PushDestination(
             filesystem=pusher_pb2.PushDestination.Filesystem(
