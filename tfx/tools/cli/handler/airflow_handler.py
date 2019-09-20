f709be4d1cb0a1a8250b950f4d7a2e55e5ab70c1
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2019-09-20 09:00:57 -0700

    1. Added get_schema method to Beam. 2. Shifted get_schema method to base_handler. 3. Added unit tests for get_schema in beam_handler_test.py 4. Added e2e test for pipeline schema command.
    PiperOrigin-RevId: 270279784

--- tfx/tools/cli/handler/airflow_handler.py
+++ tfx/tools/cli/handler/airflow_handler.py
@@ -104,54 +104,6 @@ class AirflowHandler(base_handler.BaseHandler):
     click.echo('Pipeline compiled successfully.')
     return pipeline_args
 
-  # TODO(b/140566705): Move get_schema to base_handler after testing on Beam.
-  def get_schema(self) -> None:
-    pipeline_name = self.flags_dict[labels.PIPELINE_NAME]
-
-    # Check if pipeline exists.
-    self._check_pipeline_existence(pipeline_name)
-
-    # Path to pipeline args.
-    pipeline_args_path = os.path.join(self._handler_home_dir,
-                                      self.flags_dict[labels.PIPELINE_NAME],
-                                      'pipeline_args.json')
-
-    # Get pipeline_root.
-    with open(pipeline_args_path, 'r') as f:
-      pipeline_args = json.load(f)
-
-    # Check if pipeline root created. If not, it means that the user has not
-    # created a run yet or the pipeline is still running for the first time.
-    pipeline_root = pipeline_args[labels.PIPELINE_ROOT]
-    if not tf.io.gfile.exists(pipeline_root):
-      sys.exit(
-          'Create a run before inferring schema. If pipeline is already running, then wait for it to successfully finish.'
-      )
-
-    # If pipeline_root exists, then check if SchemaGen output exists.
-    components = tf.io.gfile.listdir(pipeline_root)
-    if 'SchemaGen' not in components:
-      sys.exit(
-          'Either SchemaGen component does not exist or pipeline is still running. If pipeline is running, then wait for it to successfully finish.'
-      )
-
-    # Get the latest SchemaGen output.
-    schemagen_outputs = tf.io.gfile.listdir(
-        os.path.join(pipeline_root, 'SchemaGen', 'output', ''))
-    latest_schema_folder = max(schemagen_outputs, key=int)
-
-    # Copy schema to current dir.
-    latest_schema_path = os.path.join(pipeline_root, 'SchemaGen', 'output',
-                                      latest_schema_folder, 'schema.pbtxt')
-    curr_dir_path = os.path.join(os.getcwd(), 'schema.pbtxt')
-    io_utils.copy_file(latest_schema_path, curr_dir_path, overwrite=True)
-
-    # Print schema and path to schema
-    click.echo('Path to schema: {}'.format(curr_dir_path))
-    click.echo('*********SCHEMA FOR {}**********'.format(pipeline_name.upper()))
-    with open(curr_dir_path, 'r') as f:
-      click.echo(f.read())
-
   def create_run(self) -> None:
     """Trigger DAG in Airflow."""
     pipeline_name = self.flags_dict[labels.PIPELINE_NAME]
--- tfx/tools/cli/handler/beam_handler_test.py
+++ tfx/tools/cli/handler/beam_handler_test.py
@@ -29,16 +29,22 @@ from tfx.tools.cli.handler import beam_handler
 
 
 def _MockSubprocess(cmd, env):  # pylint: disable=invalid-name, unused-argument
-  # Store pipeline_args in a pickle file
+  # Store pipeline_args in a json file
   pipeline_args_path = env[labels.TFX_JSON_EXPORT_PIPELINE_ARGS_PATH]
-  pipeline_args = {'pipeline_name': 'chicago_taxi_beam'}
+  pipeline_name = 'chicago_taxi_beam'
+  pipeline_root = os.path.join(os.environ['HOME'], 'tfx', 'pipelines',
+                               pipeline_name)
+  pipeline_args = {
+      'pipeline_name': pipeline_name,
+      'pipeline_root': pipeline_root
+  }
   with open(pipeline_args_path, 'w') as f:
     json.dump(pipeline_args, f)
   return 0
 
 
 def _MockSubprocess2(cmd, env):  # pylint: disable=invalid-name, unused-argument
-  # Store pipeline_args in a pickle file
+  # Store pipeline_args in a json file
   pipeline_args_path = env[labels.TFX_JSON_EXPORT_PIPELINE_ARGS_PATH]
   pipeline_args = {}
   with open(pipeline_args_path, 'w') as f:
@@ -55,13 +61,16 @@ class BeamHandlerTest(tf.test.TestCase):
 
   def setUp(self):
     super(BeamHandlerTest, self).setUp()
-    self._home = os.path.join(
-        os.environ.get('TEST_UNDECLARED_OUTPUTS_DIR', self.get_temp_dir()),
-        self._testMethodName)
+    self._tmp_dir = os.environ.get('TEST_UNDECLARED_OUTPUTS_DIR',
+                                   self.get_temp_dir())
+    self._home = os.path.join(self._tmp_dir, self._testMethodName)
+    self._olddir = os.getcwd()
+    os.chdir(self._tmp_dir)
     self._original_home_value = os.environ.get('HOME', '')
     os.environ['HOME'] = self._home
     self._original_beam_home_value = os.environ.get('BEAM_HOME', '')
     os.environ['BEAM_HOME'] = os.path.join(os.environ['HOME'], 'beam')
+    self._beam_home = os.environ['BEAM_HOME']
 
     # Flags for handler.
     self.engine = 'beam'
@@ -70,6 +79,8 @@ class BeamHandlerTest(tf.test.TestCase):
     self.pipeline_path = os.path.join(self.chicago_taxi_pipeline_dir,
                                       'test_pipeline_beam_1.py')
     self.pipeline_name = 'chicago_taxi_beam'
+    self.pipeline_root = os.path.join(self._home, 'tfx', 'pipelines',
+                                      self.pipeline_name)
     self.run_id = 'dummyID'
 
     # Pipeline args for mocking subprocess
@@ -80,8 +91,11 @@ class BeamHandlerTest(tf.test.TestCase):
 
   def tearDown(self):
     super(BeamHandlerTest, self).tearDown()
-    os.environ['HOME'] = self._original_home_value
-    os.environ['BEAM_HOME'] = self._original_beam_home_value
+    if self._home:
+      os.environ['HOME'] = self._original_home_value
+    if self._beam_home:
+      os.environ['BEAM_HOME'] = self._original_beam_home_value
+    os.chdir(self._olddir)
 
   @mock.patch('subprocess.call', _MockSubprocess)
   def testSavePipeline(self):
@@ -249,6 +263,80 @@ class BeamHandlerTest(tf.test.TestCase):
     with self.captureWritesToStream(sys.stdout) as captured:
       handler.list_pipelines()
     self.assertIn('No pipelines to display.', captured.contents())
+
+  @mock.patch('subprocess.call', _MockSubprocess)
+  def testPipelineSchemaNoPipelineRoot(self):
+    flags_dict = {
+        labels.ENGINE_FLAG: self.engine,
+        labels.PIPELINE_DSL_PATH: self.pipeline_path
+    }
+    handler = beam_handler.BeamHandler(flags_dict)
+    handler.create_pipeline()
+
+    flags_dict = {
+        labels.ENGINE_FLAG: self.engine,
+        labels.PIPELINE_NAME: self.pipeline_name,
+    }
+    handler = beam_handler.BeamHandler(flags_dict)
+    with self.assertRaises(SystemExit) as err:
+      handler.get_schema()
+    self.assertEqual(
+        str(err.exception),
+        'Create a run before inferring schema. If pipeline is already running, then wait for it to successfully finish.'
+    )
+
+  @mock.patch('subprocess.call', _MockSubprocess)
+  def testPipelineSchemaNoSchemaGenOutput(self):
+    # First create a pipeline.
+    flags_dict = {
+        labels.ENGINE_FLAG: self.engine,
+        labels.PIPELINE_DSL_PATH: self.pipeline_path
+    }
+    handler = beam_handler.BeamHandler(flags_dict)
+    handler.create_pipeline()
+
+    flags_dict = {
+        labels.ENGINE_FLAG: self.engine,
+        labels.PIPELINE_NAME: self.pipeline_name,
+    }
+    handler = beam_handler.BeamHandler(flags_dict)
+    tf.io.gfile.makedirs(self.pipeline_root)
+    with self.assertRaises(SystemExit) as err:
+      handler.get_schema()
+    self.assertEqual(
+        str(err.exception),
+        'Either SchemaGen component does not exist or pipeline is still running. If pipeline is running, then wait for it to successfully finish.'
+    )
+
+  @mock.patch('subprocess.call', _MockSubprocess)
+  def testPipelineSchemaSuccessfulRun(self):
+    # First create a pipeline.
+    flags_dict = {
+        labels.ENGINE_FLAG: self.engine,
+        labels.PIPELINE_DSL_PATH: self.pipeline_path
+    }
+    handler = beam_handler.BeamHandler(flags_dict)
+    handler.create_pipeline()
+
+    flags_dict = {
+        labels.ENGINE_FLAG: self.engine,
+        labels.PIPELINE_NAME: self.pipeline_name,
+    }
+    handler = beam_handler.BeamHandler(flags_dict)
+    # Create fake schema in pipeline root.
+    schema_path = os.path.join(self.pipeline_root, 'SchemaGen', 'output', '3')
+    tf.io.gfile.makedirs(schema_path)
+    with open(os.path.join(schema_path, 'schema.pbtxt'), 'w') as f:
+      f.write('SCHEMA')
+    with self.captureWritesToStream(sys.stdout) as captured:
+      handler.get_schema()
+      curr_dir_path = os.path.join(os.getcwd(), 'schema.pbtxt')
+      self.assertIn('Path to schema: {}'.format(curr_dir_path),
+                    captured.contents())
+      self.assertIn(
+          '*********SCHEMA FOR {}**********'.format(self.pipeline_name.upper()),
+          captured.contents())
+      self.assertTrue(tf.io.gfile.exists(curr_dir_path))
 
   @mock.patch('subprocess.call', _MockSubprocess3)
   def testCreateRun(self):
