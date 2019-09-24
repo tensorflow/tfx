ceaf7a43d9668e0e62d4fb0184effcadb087c863
robertcrowe <robertcrowe@google.com>
2019-09-24 15:57:43 -0700

    Updating image to gcloud container registry and fixing init
    PiperOrigin-RevId: 271004825

--- tfx/examples/containers/workshop/buildfiles/init.sh
+++ tfx/examples/containers/workshop/buildfiles/init.sh
@@ -36,7 +36,6 @@ git clone https://github.com/tensorflow/tfx.git
 cd tfx
 git checkout -f origin/r0.14
 rm -rf tfx/examples/containers/workshop
-cd /
 
 virtualenv -p python3.6 tfx_env
 source /root/tfx_env/bin/activate
