8d7c804d4ada3caa60c031c48d43b96a5924db96
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2019-10-09 08:55:52 -0700

    no-op
    PiperOrigin-RevId: 273755280

--- tfx/orchestration/kubeflow/kubeflow_dag_runner.py
+++ tfx/orchestration/kubeflow/kubeflow_dag_runner.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""TFX runner for Kubeflow."""
+"""TFX runner for Kubeflow. No-op change for testing Kokoro."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
