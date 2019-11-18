eef831e85d5c161e2d8b669f1a09fae6d1b2ef9d
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2019-11-18 10:52:54 -0800

    Created a iris pipeline example using tuner component
    PiperOrigin-RevId: 281105294

--- tfx/examples/custom_components/tuner/tuner_component/component.py
+++ tfx/examples/custom_components/tuner/tuner_component/component.py
@@ -1,3 +1,4 @@
+# Lint as: python2, python3
 # Copyright 2019 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,9 +18,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from typing import Optional, Text, NamedTuple
 import kerastuner
 import tensorflow as tf
-from typing import Optional, Text, NamedTuple
 
 from tfx import types
 from tfx.components.base import base_component
