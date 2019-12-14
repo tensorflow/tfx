25c09cfa6026d9232946569ade8dc1b485352778
muchida <muchida@google.com>
2019-12-13 19:18:50 -0800

    Pass in image building related flags to update_pipeline command
    PiperOrigin-RevId: 285513439

--- tfx/tools/cli/commands/pipeline.py
+++ tfx/tools/cli/commands/pipeline.py
@@ -89,9 +89,8 @@ def pipeline_group() -> None:
     help='Kubernetes namespace to connect to the KFP API.')
 def create_pipeline(ctx: Context, engine: Text, pipeline_path: Text,
                     package_path: Text, build_target_image: Text,
-                    build_base_image: Text,
-                    skaffold_cmd: Text, endpoint: Text, iap_client_id: Text,
-                    namespace: Text) -> None:
+                    build_base_image: Text, skaffold_cmd: Text, endpoint: Text,
+                    iap_client_id: Text, namespace: Text) -> None:
   """Command definition to create a pipeline."""
   # TODO(b/142358865): Add support for container building for Airflow and Beam
   # runners when they support container executors.
@@ -121,6 +120,26 @@ def create_pipeline(ctx: Context, engine: Text, pipeline_path: Text,
     help='Path to the pipeline output workflow file. When unset, it will try to find the workflow file, "<pipeline_name>.tar.gz" in the current directory.'
 )
 @click.option(
+    '--build_target_image',
+    default=None,
+    type=str,
+    help='Target container image path. The target image will be built by this '
+    'command to include local python codes to the TFX default image. By default, '
+    'it uses docker daemon to build an image which will install the local '
+    'python setup file onto TFX default image. You can place a setup.py file '
+    'to control the python code to install the dependent packages. You can also '
+    'customize the Skaffold building options by placing a build.yaml in the '
+    'local directory. In addition, you can place a Dockerfile file to customize'
+    'the docker building script.')
+@click.option(
+    '--build_base_image',
+    default=None,
+    type=str,
+    help='Container image path to be used as the base image. If not specified, '
+    'target image will be build based on the released TFX image.')
+@click.option(
+    '--skaffold_cmd', default=None, type=str, help='Skaffold program command.')
+@click.option(
     '--endpoint',
     default=None,
     type=str,
@@ -137,13 +156,17 @@ def create_pipeline(ctx: Context, engine: Text, pipeline_path: Text,
     type=str,
     help='Kubernetes namespace to connect to the KFP API.')
 def update_pipeline(ctx: Context, engine: Text, pipeline_path: Text,
-                    package_path: Text, endpoint: Text, iap_client_id: Text,
-                    namespace: Text) -> None:
+                    package_path: Text, build_target_image: Text,
+                    build_base_image: Text, skaffold_cmd: Text, endpoint: Text,
+                    iap_client_id: Text, namespace: Text) -> None:
   """Command definition to update a pipeline."""
   click.echo('Updating pipeline')
   ctx.flags_dict[labels.ENGINE_FLAG] = engine
   ctx.flags_dict[labels.PIPELINE_DSL_PATH] = pipeline_path
   ctx.flags_dict[labels.PIPELINE_PACKAGE_PATH] = package_path
+  ctx.flags_dict[labels.TARGET_IMAGE] = build_target_image
+  ctx.flags_dict[labels.BASE_IMAGE] = build_base_image
+  ctx.flags_dict[labels.SKAFFOLD_CMD] = skaffold_cmd
   ctx.flags_dict[labels.ENDPOINT] = endpoint
   ctx.flags_dict[labels.IAP_CLIENT_ID] = iap_client_id
   ctx.flags_dict[labels.NAMESPACE] = namespace
