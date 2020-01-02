4aa19a99776da383bfdfcbff665101c049c53bef
jiyongjung <jiyongjung@google.com>
2020-01-02 15:20:57 -0800

    Makes buildspec test more robust using temporary directory for output.
    PiperOrigin-RevId: 287903892

--- tfx/tools/cli/container_builder/buildspec_test.py
+++ tfx/tools/cli/container_builder/buildspec_test.py
@@ -18,7 +18,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import collections
 import os
 import tensorflow as tf
 import yaml
@@ -29,35 +28,14 @@ from tfx.tools.cli.container_builder import labels
 
 class BuildSpecTest(tf.test.TestCase):
 
-  def _sort_list(self, data):
-    res = list()
-    for d in data:
-      if isinstance(d, list):
-        res.append(self._sort_list(d))
-      elif isinstance(d, dict):
-        res.append(self._sort_dict(d))
-      else:
-        res.append(d)
-    return res
-
-  def _sort_dict(self, data):
-    res = collections.OrderedDict()
-    for k, v in sorted(data.items()):
-      if isinstance(v, dict):
-        res[k] = self._sort_dict(v)
-      elif isinstance(v, list):
-        res[k] = self._sort_list(v)
-      else:
-        res[k] = v
-    return res
-
   def test_generate_clean(self):
     test_buildspec_name = 'test_buildspec'
     default_buildspec_path = os.path.join(
-        os.path.dirname(__file__), 'testdata',
-        test_buildspec_name)
+        os.path.dirname(__file__), 'testdata', test_buildspec_name)
+    output_path = os.path.join(self.create_tempdir().full_path, 'generated')
+
     build_spec = buildspec.BuildSpec.load_default(
-        filename=labels.BUILD_SPEC_FILENAME,
+        filename=output_path,
         target_image='gcr.io/test:dev',
         dockerfile_name=labels.DOCKERFILE_NAME)
     with open(default_buildspec_path, 'r') as f:
@@ -65,11 +43,8 @@ class BuildSpecTest(tf.test.TestCase):
     with open(build_spec.filename, 'r') as f:
       generated_buildspec = yaml.safe_load(f)
 
-    self.assertEqual(
-        self._sort_dict(generated_buildspec), self._sort_dict(golden_buildspec))
+    self.assertEqual(generated_buildspec, golden_buildspec)
 
-    # clean up
-    os.remove(build_spec.filename)
 
 if __name__ == '__main__':
   tf.test.main()
