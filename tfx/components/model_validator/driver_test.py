23c8e5fc250f5c5b1649b5c02c0565232b86c074
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-02-07 23:14:10 -0800

    Fix ModelValidator driver test
    PiperOrigin-RevId: 293960038

--- tfx/components/model_validator/driver_test.py
+++ tfx/components/model_validator/driver_test.py
@@ -58,12 +58,26 @@ class DriverTest(tf.test.TestCase):
         for aid in [4, 3, 2, 1]
     ]
 
-    # Mock blessing artifact produced by another component and another pipeline.
-    artifacts.extend([
-        self._create_mock_artifact(True, 5, pipeline_name,
-                                   'different_component'),
-        self._create_mock_artifact(True, 6, 'different_pipeline', component_id)
-    ])
+    # Mock blessing artifact produced by another component.
+    artifacts.append(
+        self._create_mock_artifact(
+            aid=5,
+            is_blessed=True,
+            pipeline_name=pipeline_name,
+            component_id='different_component'))
+
+    mock_metadata.get_artifacts_by_type.return_value = artifacts
+    self.assertEqual(('uri-3', 3),
+                     model_validator_driver._fetch_last_blessed_model(
+                         pipeline_name, component_id))
+
+    # Mock blessing artifact produced by another pipeline.
+    artifacts.append(
+        self._create_mock_artifact(
+            aid=6,
+            is_blessed=True,
+            pipeline_name='different_pipeline',
+            component_id=component_id))
 
     mock_metadata.get_artifacts_by_type.return_value = artifacts
     self.assertEqual(('uri-3', 3),
