f5bbf0dad9803baddf2d6a4a4f087bbff972fab6
ananthr <ananthr@google.com>
2020-02-13 15:19:06 -0800

    TFX OSS MNIST Native Keras Example.
    PiperOrigin-RevId: 295012522

---
+++ tfx/examples/mnist/mnist_utils_native_keras.py
@@ -0,0 +1,172 @@
+# Lint as: python2, python3
+# Copyright 2019 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Python source file include MNIST pipeline functions and necessary utils.
+
+The utilities in this file are used to build a model with native Keras.
+This module file will be used in Transform and generic Trainer.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from typing import Text
+
+import absl
+import tensorflow as tf
+from tensorflow.keras import layers
+import tensorflow_transform as tft
+
+from tfx.components.trainer.executor import TrainerFnArgs
+
+# MNIST dataset consists of an image of the handwritten digits,
+# and it's label which is the class indicating digits 0 through 9.
+_IMAGE_KEY = 'image/floats'
+_LABEL_KEY = 'image/class'
+
+
+def _transformed_name(key):
+  return key + '_xf'
+
+
+def _gzip_reader_fn(filenames):
+  """Small utility returning a record reader that can read gzip'ed files."""
+  return tf.data.TFRecordDataset(filenames, compression_type='GZIP')
+
+
+def _get_serve_tf_examples_fn(model, tf_transform_output):
+  """Returns a function that parses a serialized tf.Example."""
+
+  @tf.function
+  def serve_tf_examples_fn(serialized_tf_examples):
+    """Returns the output to be used in the serving signature."""
+    feature_spec = tf_transform_output.raw_feature_spec()
+    feature_spec.pop(_LABEL_KEY)
+    parsed_features = tf.io.parse_example(serialized_tf_examples, feature_spec)
+
+    transformed_features = tf_transform_output.transform_raw_features(
+        parsed_features)
+    transformed_features.pop(_transformed_name(_LABEL_KEY))
+
+    outputs = model(transformed_features)
+    return {'outputs': outputs}
+
+  return serve_tf_examples_fn
+
+
+def _input_fn(file_pattern: Text,
+              tf_transform_output: tft.TFTransformOutput,
+              batch_size: int = 200) -> tf.data.Dataset:
+  """Generates features and label for tuning/training.
+
+  Args:
+    file_pattern: input tfrecord file pattern.
+    tf_transform_output: A TFTransformOutput.
+    batch_size: representing the number of consecutive elements of returned
+      dataset to combine in a single batch
+
+  Returns:
+    A dataset that contains (features, indices) tuple where features is a
+      dictionary of Tensors, and indices is a single Tensor of label indices.
+  """
+  transformed_feature_spec = (
+      tf_transform_output.transformed_feature_spec().copy())
+
+  dataset = tf.data.experimental.make_batched_features_dataset(
+      file_pattern=file_pattern,
+      batch_size=batch_size,
+      features=transformed_feature_spec,
+      reader=_gzip_reader_fn,
+      label_key=_transformed_name(_LABEL_KEY))
+
+  return dataset
+
+
+def _build_keras_model() -> tf.keras.Model:
+  """Creates a DNN Keras model for classifying MNIST data.
+
+  Returns:
+    A Keras Model.
+  """
+  # The model below is built with Sequential API, please refer to
+  # https://www.tensorflow.org/guide/keras/overview for all API options.
+  model = tf.keras.Sequential()
+  model.add(layers.InputLayer(input_shape=(784,),
+                              name=_transformed_name(_IMAGE_KEY)))
+  model.add(layers.Dense(64, activation='relu'))
+  model.add(layers.Dropout(0.2))
+  model.add(layers.Dense(64, activation='relu'))
+  model.add(layers.Dropout(0.2))
+  model.add(layers.Dense(10, activation='softmax'))
+  model.compile(
+      loss='sparse_categorical_crossentropy',
+      optimizer=tf.keras.optimizers.RMSprop(lr=0.0015),
+      metrics=['sparse_categorical_accuracy']
+  )
+  absl.logging.info(model.summary())
+  return model
+
+
+# TFX Transform will call this function.
+def preprocessing_fn(inputs):
+  """tf.transform's callback function for preprocessing inputs.
+
+  Args:
+    inputs: map from feature keys to raw not-yet-transformed features.
+
+  Returns:
+    Map from string feature key to transformed feature operations.
+  """
+  outputs = {}
+
+  # The input float values for the image encoding are in the range [-0.5, 0.5].
+  # So scale_by_min_max is a identity operation, since the range is preserved.
+  outputs[_transformed_name(_IMAGE_KEY)] = (
+      tft.scale_by_min_max(inputs[_IMAGE_KEY], -0.5, 0.5))
+  outputs[_transformed_name(_LABEL_KEY)] = inputs[_LABEL_KEY]
+
+  return outputs
+
+
+# TFX Trainer will call this function.
+def run_fn(fn_args: TrainerFnArgs):
+  """Train the model based on given args.
+
+  Args:
+    fn_args: Holds args used to train the model as name/value pairs.
+  """
+  tf_transform_output = tft.TFTransformOutput(fn_args.transform_output)
+
+  train_dataset = _input_fn(fn_args.train_files, tf_transform_output, 40)
+  eval_dataset = _input_fn(fn_args.eval_files, tf_transform_output, 40)
+
+  model = _build_keras_model()
+
+  model.fit(
+      train_dataset,
+      steps_per_epoch=fn_args.train_steps,
+      validation_data=eval_dataset,
+      validation_steps=fn_args.eval_steps)
+
+  signatures = {
+      'serving_default':
+          _get_serve_tf_examples_fn(model,
+                                    tf_transform_output).get_concrete_function(
+                                        tf.TensorSpec(
+                                            shape=[None],
+                                            dtype=tf.string,
+                                            name='examples')),
+  }
+  model.save(fn_args.serving_model_dir, save_format='tf', signatures=signatures)
