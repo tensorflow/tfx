f430d97219a106c6fab6e10b69c197c8d4ad6177
robertcrowe <robertcrowe@google.com>
2020-02-28 09:04:00 -0800

    Updating Airflow workshop to TFX 0.21
    PiperOrigin-RevId: 297856011

--- tfx/examples/airflow_workshop/setup/setup_demo.sh
+++ tfx/examples/airflow_workshop/setup/setup_demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2018 Google LLC
+# Copyright 2020 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,13 +31,10 @@ pip install httplib2==0.12.0
 printf "${GREEN}Installing pendulum to avoid problem with tzlocal${NORMAL}\n"
 pip install pendulum==1.4.4
 
-# TODO(b/135470014): Use range or pin for pip installs.
-# TF/TFX prereqs
-printf "${GREEN}Installing TensorFlow${NORMAL}\n"
-pip install tensorflow==1.14.0
-
 printf "${GREEN}Installing TFX${NORMAL}\n"
-pip install tfx==0.14.0rc1
+pip install pyarrow==0.15.0
+pip install apache_beam==2.17.0
+pip install tfx==0.21.0
 
 printf "${GREEN}Installing Google API Client${NORMAL}\n"
 pip install google-api-python-client
@@ -64,9 +61,7 @@ pip install docker
 printf "${GREEN}Preparing environment for Airflow${NORMAL}\n"
 export SLUGIFY_USES_TEXT_UNIDECODE=yes
 printf "${GREEN}Installing Airflow${NORMAL}\n"
-# TODO(b/136777165): Remove pinned version of Flask and Werkzeug
-# after newer version of Airflow: see AIRFLOW-4900.
-pip install apache-airflow==1.10.3 Flask==1.0.4 Werkzeug==0.14.1
+pip install -q apache-airflow==1.10.9 Flask==1.1.1 Werkzeug==0.15
 printf "${GREEN}Initializing Airflow database${NORMAL}\n"
 airflow initdb
 
@@ -83,22 +78,14 @@ printf "${GREEN}Refreshing Airflow to pick up new config${NORMAL}\n"
 airflow resetdb --yes
 airflow initdb
 
-# Copy Dag to ~/airflow/dags
+# Copy Dags to ~/airflow/dags
 mkdir -p ~/airflow/dags
 cp dags/taxi_pipeline.py ~/airflow/dags/
 cp dags/taxi_utils.py ~/airflow/dags/
-
-# Copy the simple pipeline example and adjust for user's environment
-cp ../../chicago_taxi_pipeline/taxi_pipeline_simple.py ~/airflow/dags/taxi_pipeline_solution.py
-cp ../../chicago_taxi_pipeline/taxi_utils.py ~/airflow/dags/taxi_utils_solution.py
-sed -i'.orig' "s/os.environ\['HOME'\], 'taxi'/os.environ\['HOME'\], 'airflow'/g" ~/airflow/dags/taxi_pipeline_solution.py
-sed -i'.orig' "s/_taxi_root, 'data', 'simple'/_taxi_root, 'data', 'taxi_data'/g" ~/airflow/dags/taxi_pipeline_solution.py
-sed -i'.orig' "s/taxi_utils.py/dags\/taxi_utils_solution.py/g" ~/airflow/dags/taxi_pipeline_solution.py
-sed -i'.orig' "s/os.environ\['HOME'\], 'tfx'/_taxi_root, 'tfx'/g" ~/airflow/dags/taxi_pipeline_solution.py
-sed -i'.orig' "s/chicago_taxi_simple/taxi_solution/g" ~/airflow/dags/taxi_pipeline_solution.py
+cp dags/taxi_pipeline_solution.py ~/airflow/dags/
+cp dags/taxi_utils_solution.py ~/airflow/dags/
 
 # Copy data to ~/airflow/data
-# TODO(b/130311020): Combine Chicago Taxi data files
 cp -R data ~/airflow
 
 printf "\n${GREEN}TFX workshop installed${NORMAL}\n"
