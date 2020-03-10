b8bbe748c3ee84045d1d5d1d9ce3cdd4ebd52c17
jjong <jjong@google.com>
2020-03-10 00:48:59 -0700

    Add multiple flavors of model path.
    Each model path flavor module implements
    (1) make_model_path() and parse_model_path() for manipulating model path.
    (2) Optionally lookup_model_paths() and lookup_only_model_path() if needed.
    
    path_utils is now implemented on top of this.
    
    PiperOrigin-RevId: 300032900

--- tfx/utils/model_paths/tf_serving_flavor.py
+++ tfx/utils/model_paths/tf_serving_flavor.py
@@ -14,20 +14,21 @@
 # limitations under the License.
 """Module for constructing and parsing tensorflow-serving-flavored model path.
 
-In TensorFlow Serving, model should be stored under directory structure
-`{base_path}/{model_name}/{version:int}` to be correctly recognized from the
-model server. We call it tensorflow-serving-flavored (or TFS-flavored) model
-path.
+In TensorFlow Serving, the model should be stored with the directory structure
+`{model_base_path}/{model_name}/{version}` to be correctly recognized by the
+model server. We call this a *TFS-flavored model path*.
+
+Example:
 
 ```
-/foo/bar/        # base_path
-  my_model/      # model_name
-    1582072718/  # version
-      (Your exported model)
+/foo/bar/        # A `model_base_path`
+  my_model/      # A `model_name`
+    1582072718/  # An integer `version`
+      (Your exported SavedModel)
 ```
 
-If you're using TensorFlow estimator API, exporter use this directory structure
-to organize the model, where `model_name` is the name of the exporter.
+If you're using TensorFlow estimator API, the exporter uses this directory
+structure to organize the model, where `model_name` is the name of the exporter.
 
 TensorFlow Serving also requires the version segment to be an integer (mostly
 a unix timestamp) to track the latest model easily.
@@ -41,21 +42,19 @@ import os
 from typing import Optional, Text, Tuple
 
 
-# TODO(b/149534564): Replace os.path to pathlib after py2 deprecation.
-def make_model_path(base_path: Text, model_name: Text, version: int) -> Text:
+def make_model_path(model_base_path: Text, model_name: Text,
+                    version: int) -> Text:
   """Make a TFS-flavored model path.
 
   Args:
-    base_path: A base path containig the directory of model_name.
+    model_base_path: A base path containing the directory of model_name.
     model_name: A name of the model.
     version: An integer version of the model.
 
   Returns:
-    `{base_path}/{model_name}/{version}`.
+    `{model_base_path}/{model_name}/{version}`.
   """
-  if isinstance(version, str) and not version.isdigit():
-    raise ValueError('version should be an integer, got {}'.format(version))
-  return os.path.join(base_path, model_name, str(version))
+  return os.path.join(model_base_path, model_name, str(version))
 
 
 def parse_model_path(
@@ -64,41 +63,41 @@ def parse_model_path(
   """Parse model_path into parts of TFS flavor.
 
   Args:
-    model_path: An TFS-flavored model path.
+    model_path: A TFS-flavored model path.
     expected_model_name: Expected model_name as defined from the module
-        docstring. If model_name does not match, parse will be failed.
+        docstring. If model_name does not match, parsing will be failed.
 
   Raises:
     ValueError: If model path is invalid (not TFS-flavored).
 
   Returns:
-    Tuple of (base_path, model_name, version)
+    Tuple of (model_base_path, model_name, version)
   """
   rest, version = os.path.split(model_path)
   if not rest:
     raise ValueError('model_path is too short ({})'.format(model_path))
-  if not version or not version.isdigit():
+  if not version.isdigit():
     raise ValueError('No version segment ({})'.format(model_path))
   version = int(version)
 
-  base_path, model_name = os.path.split(rest)
+  model_base_path, model_name = os.path.split(rest)
   if expected_model_name is not None and model_name != expected_model_name:
     raise ValueError('model_name does not match (expected={}, actual={})'
                      .format(expected_model_name, model_path))
 
-  return base_path, model_name, version
+  return model_base_path, model_name, version
 
 
-def parse_base_path(model_path: Text) -> Text:
-  """Parse base_path from the TFS-flavored model path.
+def parse_model_base_path(model_path: Text) -> Text:
+  """Parse model_base_path from the TFS-flavored model path.
 
   Args:
-    model_path: An TFS-flavored model path.
+    model_path: A TFS-flavored model path.
 
   Raises:
     ValueError: If model path is invalid (not TFS-flavored).
 
   Returns:
-    base_path as defined from the module docstring.
+    model_base_path as defined from the module docstring.
   """
   return parse_model_path(model_path)[0]
