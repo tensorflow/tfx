6fdfb23941917cb69502e34b5db75765a32abbfd
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-03-26 15:35:27 -0700

    Make _FakeDriver more generic. It can now work with multi-output components and channels with multiple artifacts.
    PiperOrigin-RevId: 303207967

--- tfx/orchestration/launcher/base_component_launcher_test.py
+++ tfx/orchestration/launcher/base_component_launcher_test.py
@@ -77,7 +77,7 @@ class ComponentRunnerTest(tf.test.TestCase):
         ]))
     launcher.launch()
 
-    output_path = os.path.join(pipeline_root, 'output')
+    output_path = component.outputs['output'].get()[0].uri
     self.assertTrue(tf.io.gfile.exists(output_path))
     contents = file_io.read_file_to_string(output_path)
     self.assertEqual('test', contents)
