1689196357424b64cf98e67bf450d6a525ebc717
jjong <jjong@google.com>
2020-04-23 20:57:39 -0700

    Refactor Pusher to be easily extensible.
    PiperOrigin-RevId: 308187867

--- tfx/components/pusher/executor.py
+++ tfx/components/pusher/executor.py
@@ -18,11 +18,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import abc
 import os
 import time
 from typing import Any, Dict, List, Optional, Text
 
 from absl import logging
+import six
 import tensorflow as tf
 
 from google.protobuf import json_format
@@ -45,6 +47,10 @@ MODEL_BLESSING_KEY = 'model_blessing'
 INFRA_BLESSING_KEY = 'infra_blessing'
 # Key for pushed model in executor output_dict.
 PUSHED_MODEL_KEY = 'pushed_model'
+# Key for PushDestination in exec_properties.
+PUSH_DESTINATION_KEY = 'push_destination'
+# Key for custom_config in exec_properties.
+CUSTOM_CONFIG_KEY = 'custom_config'
 
 # Key for PushedModel artifact properties.
 _PUSHED_KEY = 'pushed'
@@ -52,25 +58,37 @@ _PUSHED_DESTINATION_KEY = 'pushed_destination'
 _PUSHED_VERSION_KEY = 'pushed_version'
 
 
-class Executor(base_executor.BaseExecutor):
+# TODO(jjong): Use dataclasses or attrs instead?
+class PushResult(object):
+  """Data class for push result."""
+  __slots__ = ['destination', 'version']
+
+  def __init__(self, destination: Text, version: Optional[Text] = None):
+    self.destination = destination
+    self.version = version
+
+
+class BasePusherExecutor(six.with_metaclass(abc.ABCMeta,
+                                            base_executor.BaseExecutor)):
   """TFX Pusher executor to push the new TF model to a filesystem target.
 
   The Pusher component is used to deploy a validated model to a filesystem
-  target or serving environment using tf.serving.  Pusher depends on the outputs
+  target or serving environment using tf.serving. Pusher depends on the outputs
   of ModelValidator to determine if a model is ready to push. A model is
   considered to be safe to push only if ModelValidator has marked it as BLESSED.
   A push action delivers the model exports produced by Trainer to the
-  destination defined in the ``push_destination`` of the component config.
+  destination defined in the `push_destination` of the component config.
 
   To include Pusher in a TFX pipeline, configure your pipeline similar to
   https://github.com/tensorflow/tfx/blob/master/tfx/examples/chicago_taxi_pipeline/taxi_pipeline_simple.py#L104.
 
   For more details on tf.serving itself, please refer to
-  https://tensorflow.org/tfx/guide/pusher.  For a tutuorial on TF Serving,
+  https://tensorflow.org/tfx/guide/pusher. For a tutorial on TF Serving,
   please refer to https://www.tensorflow.org/tfx/guide/serving.
   """
 
-  def CheckBlessing(self, input_dict: Dict[Text, List[types.Artifact]]) -> bool:
+  def _CheckBlessing(
+      self, input_dict: Dict[Text, List[types.Artifact]]) -> bool:
     """Check that model is blessed by upstream validators.
 
     Args:
@@ -111,7 +129,7 @@ class Executor(base_executor.BaseExecutor):
     Args:
       input_dict: Input dict from input key to a list of artifacts, including:
         - model_export: exported model from trainer.
-        - model_blessing: model blessing path from model_validator.  A push
+        - model_blessing: model blessing path from model_validator. A push
           action delivers the model exports produced by Trainer to the
           destination defined in component config.
       output_dict: Output dict from key to a list of artifacts, including:
@@ -127,54 +145,61 @@ class Executor(base_executor.BaseExecutor):
     self._log_startup(input_dict, output_dict, exec_properties)
     model_push = artifact_utils.get_single_instance(
         output_dict[PUSHED_MODEL_KEY])
-    if not self.CheckBlessing(input_dict):
+
+    if not self._CheckBlessing(input_dict):
       self._MarkNotPushed(model_push)
       return
+
     model_export = artifact_utils.get_single_instance(input_dict[MODEL_KEY])
-    model_path = path_utils.serving_model_path(model_export.uri)
 
-    # Push model to the destination, which can be listened by a model server.
-    #
-    # If model is already successfully copied to outside before, stop copying.
-    # This is because model validator might blessed same model twice (check
-    # mv driver) with different blessing output, we still want Pusher to
-    # handle the mv output again to keep metadata tracking, but no need to
-    # copy to outside path again..
-    # TODO(jyzhao): support rpc push and verification.
-    push_destination = pusher_pb2.PushDestination()
-    json_format.Parse(exec_properties['push_destination'], push_destination)
+    # Copy Model artifact contents to the PushedModel artifact. Note that
+    # although the contents of Model and PushedModel artifacts looks the same,
+    # they serve the different purpose, and it is more appropriate to have two
+    # copies of the model in each artifact. Some future considerations:
+    # 1) Each artifact might have different GC policy in the future, and we
+    #    don't want to have dangling deployment due to deletion of Model
+    #    artifact (which is a Trainer output).
+    # 2) PushedModel artifact might rewrite the input model in the future.
+    io_utils.copy_dir(path_utils.serving_model_path(model_export.uri),
+                      model_push.uri)
 
-    destination_kind = push_destination.WhichOneof('destination')
-    if destination_kind == 'filesystem':
-      fs_config = push_destination.filesystem
-      if fs_config.versioning == _Versioning.AUTO:
-        fs_config.versioning = _Versioning.UNIX_TIMESTAMP
-      if fs_config.versioning == _Versioning.UNIX_TIMESTAMP:
-        model_version = str(int(time.time()))
-      else:
-        raise NotImplementedError(
-            'Invalid Versioning {}'.format(fs_config.versioning))
-      logging.info('Model version: %s', model_version)
-      serving_path = os.path.join(fs_config.base_directory, model_version)
+    push_dest = pusher_pb2.PushDestination()
+    serialized_push_dest = exec_properties.get(PUSH_DESTINATION_KEY)
+    if serialized_push_dest:
+      json_format.Parse(serialized_push_dest, push_dest)
+    # TODO(jjong): Why is custom_config not serialized?
+    custom_config = exec_properties.get(CUSTOM_CONFIG_KEY, {})
 
-      if tf.io.gfile.exists(serving_path):
-        logging.info(
-            'Destination directory %s already exists, skipping current push.',
-            serving_path)
-      else:
-        # tf.serving won't load partial model, it will retry until fully copied.
-        io_utils.copy_dir(model_path, serving_path)
-        logging.info('Model written to serving path %s.', serving_path)
+    try:
+      result = self._PushImpl(
+          model_push=model_push,
+          push_destination=push_dest,
+          custom_config=custom_config,
+      )
+    except:  # pylint: disable=broad-except, bare-except
+      self._MarkNotPushed(model_push)
     else:
-      raise NotImplementedError(
-          'Invalid push destination {}'.format(destination_kind))
+      self._MarkPushed(model_push,
+                       pushed_destination=result.destination,
+                       pushed_version=result.version)
 
-    # Copy the model to pushing uri for archiving.
-    io_utils.copy_dir(model_path, model_push.uri)
-    self._MarkPushed(model_push,
-                     pushed_destination=serving_path,
-                     pushed_version=model_version)
-    logging.info('Model pushed to %s.', model_push.uri)
+  @abc.abstractmethod
+  def _PushImpl(self, model_push: types.Artifact,
+                push_destination: pusher_pb2.PushDestination,
+                custom_config: Dict[Text, Any]) -> PushResult:
+    """Implementation of the push behavior.
+
+    Args:
+      model_push: PushedModel artifact that contains the model to be deployed.
+      push_destination: Config about push destination.
+      custom_config: Custom configuration for custom push destination.
+
+    Returns:
+      PushResult.
+
+    Raises:
+      NotImplementedError: If given push_destination is not implemented.
+    """
 
   def _MarkPushed(self, model_push: types.Artifact, pushed_destination: Text,
                   pushed_version: Optional[Text] = None) -> None:
@@ -182,7 +207,51 @@ class Executor(base_executor.BaseExecutor):
     model_push.set_string_custom_property(
         _PUSHED_DESTINATION_KEY, pushed_destination)
     if pushed_version is not None:
-      model_push.set_string_custom_property(_PUSHED_VERSION_KEY, pushed_version)
+      model_push.set_string_custom_property(
+          _PUSHED_VERSION_KEY, pushed_version)
+    logging.info('Model pushed to %s.', model_push.uri)
 
   def _MarkNotPushed(self, model_push: types.Artifact):
     model_push.set_int_custom_property('pushed', 0)
+    logging.info('Model was not pushed.')
+
+
+class Executor(BasePusherExecutor):
+  """Pusher Executor implementation for the filesystem destination."""
+
+  # TODO(jyzhao): support rpc push and verification.
+  def _PushImpl(self, model_push: types.Artifact,
+                push_destination: pusher_pb2.PushDestination,
+                custom_config: Dict[Text, Any]) -> PushResult:
+    del custom_config  # Unused.
+
+    dest = push_destination.WhichOneof('destination')
+    if dest != 'filesystem':
+      raise NotImplementedError('Unsupported push destination {}'.format(dest))
+
+    config = push_destination.filesystem
+    if config.versioning == _Versioning.AUTO:
+      config.versioning = _Versioning.UNIX_TIMESTAMP
+    if config.versioning == _Versioning.UNIX_TIMESTAMP:
+      version = str(int(time.time()))
+    else:
+      raise NotImplementedError(
+          'Invalid Versioning {}'.format(config.versioning))
+    logging.info('Model version: %s', version)
+    serving_path = os.path.join(config.base_directory, version)
+
+    if tf.io.gfile.exists(serving_path):
+      # If model is already successfully copied to outside before, stop copying.
+      # This is because model validator might blessed same model twice (check
+      # mv driver) with different blessing output, we still want Pusher to
+      # handle the mv output again to keep metadata tracking, but no need to
+      # copy to outside path again..
+      logging.info(
+          'Destination directory %s already exists, skipping current push.',
+          serving_path)
+    else:
+      # tf.serving won't load partial model, it will retry until fully copied.
+      io_utils.copy_dir(model_push.uri, serving_path)
+      logging.info('Model written to serving path %s.', serving_path)
+
+    return PushResult(destination=serving_path, version=version)
