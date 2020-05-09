9d262389ae53cda686a5e2f52e03826634603d56
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-05-08 22:11:19 -0700

    internal
    PiperOrigin-RevId: 310683912

--- tfx/orchestration/kubeflow/kubeflow_gcp_perf_test.py
+++ tfx/orchestration/kubeflow/kubeflow_gcp_perf_test.py
@@ -260,7 +260,7 @@ class KubeflowGcpPerfTest(test_utils.BaseKubeflowTest):
         beam_pipeline_args=_BEAM_PIPELINE_ARGS)
     self._compile_and_run_pipeline(
         pipeline=pipeline,
-        query_sample_rate=1,
+        query_sample_rate=0.0001,
         # (1M * batch_size=200) / 200M records ~ 1 epoch
         train_steps=1000000,
         eval_steps=10000,
