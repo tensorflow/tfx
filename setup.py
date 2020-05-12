ac5a6c1817f8c174c3a45b58b45e5305821e1c27
jjong <jjong@google.com>
2020-05-12 04:45:11 -0700

    Move static package metadata from setup.py to setup.cfg.
    PiperOrigin-RevId: 311101347

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@
 from __future__ import print_function
 
 from distutils import spawn
+from distutils.command import build_py
+from distutils.command import clean
 import glob
 import os
 import subprocess
@@ -25,126 +27,118 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-# Find the Protocol Compiler.
-if 'PROTOC' in os.environ and os.path.exists(os.environ['PROTOC']):
-  protoc = os.environ['PROTOC']
-elif os.path.exists('../src/protoc'):
-  protoc = '../src/protoc'
-elif os.path.exists('../src/protoc.exe'):
-  protoc = '../src/protoc.exe'
-elif os.path.exists('../vsprojects/Debug/protoc.exe'):
-  protoc = '../vsprojects/Debug/protoc.exe'
-elif os.path.exists('../vsprojects/Release/protoc.exe'):
-  protoc = '../vsprojects/Release/protoc.exe'
-else:
-  protoc = spawn.find_executable('protoc')
+from tfx import dependencies
 
 
-def generate_proto(source):
-  """Invokes the Protocol Compiler to generate a _pb2.py."""
+def _find_proto_compiler():
+  """Find protoc compiler binary path."""
 
-  output = source.replace('.proto', '_pb2.py')
-
-  if (not os.path.exists(output) or
-      (os.path.exists(source) and
-       os.path.getmtime(source) > os.path.getmtime(output))):
-    print('Generating %s...' % output)
-
-    if not os.path.exists(source):
-      sys.stderr.write('Cannot find required file: %s\n' % source)
-      sys.exit(-1)
-
-    if protoc is None:
-      sys.stderr.write(
-          'protoc is not installed nor found in ../src.  Please compile it '
-          'or install the binary package.\n')
-      sys.exit(-1)
-
-    protoc_command = [protoc, '-I.', '--python_out=.', source]
-    if subprocess.call(protoc_command) != 0:
-      sys.exit(-1)
+  if 'PROTOC' in os.environ and os.path.exists(os.environ['PROTOC']):
+    return os.environ['PROTOC']
+  elif os.path.exists('../src/protoc'):
+    return os.path.realpath('../src/protoc')
+  elif os.path.exists('../src/protoc.exe'):
+    return os.path.realpath('../src/protoc.exe')
+  elif os.path.exists('../vsprojects/Debug/protoc.exe'):
+    return os.path.realpath('../vsprojects/Debug/protoc.exe')
+  elif os.path.exists('../vsprojects/Release/protoc.exe'):
+    return os.path.realpath('../vsprojects/Release/protoc.exe')
+  else:
+    return spawn.find_executable('protoc')
 
 
-_PROTO_FILE_PATTERNS = [
-    'tfx/proto/*.proto',
-    'tfx/orchestration/kubeflow/proto/*.proto',
-]
+class _ProtoCompilerMixin(object):
+  """Mixins for generating py proto stubs from proto file."""
+  _protoc = _find_proto_compiler()
 
-for file_pattern in _PROTO_FILE_PATTERNS:
-  for proto_file in glob.glob(file_pattern):
-    generate_proto(proto_file)
+  def generate_proto(self, source):
+    """Invokes the Protocol Compiler to generate a _pb2.py.
 
-# Get various package dependencies list.
-with open('tfx/dependencies.py') as fp:
-  globals_dict = {}
-  exec(fp.read(), globals_dict)  # pylint: disable=exec-used
-_make_required_install_packages = globals_dict['make_required_install_packages']
-_make_extra_packages_docker_image = globals_dict[
-    'make_extra_packages_docker_image']
-_make_all_dependency_packages = globals_dict['make_all_dependency_packages']
+    Args:
+      source: path to the proto source file (endswith ".proto").
+    """
+    output = source.replace('.proto', '_pb2.py')
 
-# Get version from version module.
-with open('tfx/version.py') as fp:
-  globals_dict = {}
-  exec(fp.read(), globals_dict)  # pylint: disable=exec-used
-__version__ = globals_dict['__version__']
+    if (not os.path.exists(output) or
+        (os.path.exists(source) and
+         os.path.getmtime(source) > os.path.getmtime(output))):
+      print('Generating %s...' % output, file=sys.stderr)
 
-# Get the long description from the README file.
-with open('README.md') as fp:
-  _LONG_DESCRIPTION = fp.read()
+      if not os.path.exists(source):
+        sys.stderr.write('Cannot find required file: %s\n' % source)
+        sys.exit(-1)
+
+      if self._protoc is None:
+        sys.stderr.write(
+            'protoc is not installed nor found in ../src.  Please compile it '
+            'or install the binary package.\n')
+        sys.exit(-1)
+
+      protoc_command = [self._protoc, '-I.', '--python_out=.', source]
+      if subprocess.call(protoc_command) != 0:
+        sys.exit(-1)
+
+
+class _BuildPyCmd(build_py.build_py, _ProtoCompilerMixin):
+  """BuildPy command with python proto stub generation."""
+
+  _PROTO_FILE_PATTERNS = (
+      'tfx/proto/*.proto',
+      'tfx/orchestration/kubeflow/proto/*.proto',
+  )
+
+  def run(self):
+    # Pre-command hook.
+    # Generate proto python stubs (*_pb2.py).
+    for file_pattern in self._PROTO_FILE_PATTERNS:
+      for proto_file in glob.glob(file_pattern):
+        self.generate_proto(proto_file)
+
+    # Run build_py. It is an old-style class and super() doesn't work.
+    build_py.build_py.run(self)
+
+
+class _CleanCmd(clean.clean):
+  """Clean command with python proto stub cleanup."""
+
+  def run(self):
+    # Pre-command hook.
+    # Remove proto python stubs (*_pb2.py).
+    for root, _, files in os.walk('.'):
+      for name in files:
+        if name.endswith('_pb2.py'):
+          os.remove(os.path.join(root, name))
+
+    # Run clean. It is an old-style class and super() doesn't work.
+    clean.clean.run(self)
+
 
 setup(
-    name='tfx',
-    version=__version__,
-    author='Google LLC',
-    author_email='tensorflow-extended-dev@googlegroups.com',
-    license='Apache 2.0',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
     namespace_packages=[],
-    install_requires=_make_required_install_packages(),
+    install_requires=dependencies.make_required_install_packages(),
     extras_require={
         # In order to use 'docker-image' or 'all', system libraries specified
         # under 'tfx/tools/docker/Dockerfile' are required
-        'docker-image': _make_extra_packages_docker_image(),
-        'all': _make_all_dependency_packages(),
+        'docker-image': dependencies.make_extra_packages_docker_image(),
+        'all': dependencies.make_all_dependency_packages(),
     },
     setup_requires=['pytest-runner'],
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,<4',
     packages=find_packages(),
     include_package_data=True,
-    description='TensorFlow Extended (TFX) is a TensorFlow-based general-purpose machine learning platform implemented at Google',
-    long_description=_LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    keywords='tensorflow tfx',
-    url='https://www.tensorflow.org/tfx',
-    download_url='https://github.com/tensorflow/tfx/tags',
     requires=[],
+    cmdclass={
+        'clean': _CleanCmd,
+        'build_py': _BuildPyCmd,
+    },
     # Below console_scripts, each line identifies one console script. The first
     # part before the equals sign (=) which is 'tfx', is the name of the script
     # that should be generated, the second part is the import path followed by a
     # colon (:) with the Click command group. After installation, the user can
     # invoke the CLI using "tfx <command_group> <sub_command> <flags>"
-    entry_points="""
-        [console_scripts]
-        tfx=tfx.tools.cli.cli_main:cli_group
-    """)
+    entry_points={
+        'console_scripts': [
+            'tfx = tfx.tools.cli.cli_main:cli_group'
+        ],
+    },
+)
