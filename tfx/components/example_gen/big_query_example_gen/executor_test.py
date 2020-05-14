66b94070a0d70308f7037fef6b9cf770ebe42212
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-05-14 09:24:21 -0700

    PR #1731: Add support for BigQuery arrays in BigQueryExampleGen and update test
    Please approve this CL. It will be submitted automatically, and its GitHub pull request will be marked as merged.
    
    Imported from GitHub PR #1731
    
    When trying to construct a pipeline having an array as input (such as a time series) you'll get the error
    `TypeError: [20.592889320938852, 20.514573172830033, 19.76807155325737, 20.467801775051797, 20.5308510931856, 20 has type list, but expected one of: int, long, float [while running 'InputToSerializedExample/InputSourceToExample/ToTFExample']`
    
    This is due to incomplete checks in BigQueryExampleGen as the schema type will still be float even if the value is a list (repeatable)
    
    `SchemaField('history', 'FLOAT', 'REPEATED', None, ())`
    
    This is a quick fix that adds support for arrays of the already supported types. You might want to reconsider the general parsing of BigQuery rows, but that seem to require a better understanding of both BigQuery and TFX.
    
    Copybara import of the project:
    
      - a2bedeac106c17364b94b807fa11542eb9f00e99 Add support for BigQuery arrays in BigQueryExampleGen and... by Mattias Patricks <mattiasp@spotify.com>
      - b66ad84e9fd216b78bc72ea5bc731d027a3b3f4d Merge branch 'master' into add-bq-array-support by domedagen <lemisanthrope01@live.com>
      - 3ac7b24649ba46acb920d67dbbf1f0ca0fd78f30 Change schema mode for arrays to REPEATED and NULLABLE in... by Mattias Patricks <mattiasp@spotify.com>
      - c9cde3ef8bb096cc0d0cd70361ce17c76f64346b Arrays are always REPEATED irregardless if theyre null or... by Mattias Patricks <mattiasp@spotify.com>
      - c183eefa3372655d3090982609ec8a74f61bd055 Merge c9cde3ef8bb096cc0d0cd70361ce17c76f64346b into d227d... by domedagen <25223618+domedagen@users.noreply.github.com>
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tfx/pull/1731 from domedagen:add-bq-array-support c9cde3ef8bb096cc0d0cd70361ce17c76f64346b
    PiperOrigin-RevId: 311546313

--- tfx/components/example_gen/big_query_example_gen/executor_test.py
+++ tfx/components/example_gen/big_query_example_gen/executor_test.py
@@ -51,9 +51,12 @@ def _MockReadFromBigQuery(pipeline, query, project, use_bigquery_source):  # pyl
 def _MockReadFromBigQuery2(pipeline, query, project, use_bigquery_source):  # pylint: disable=invalid-name, unused-argument
   mock_query_results = [{
       'i': 1,
+      'i2': [2, 3],
       'b': True,
       'f': 2.0,
+      'f2': [2.7, 3.8],
       's': 'abc',
+      's2': ['abc', 'def']
   }]
   return pipeline | beam.Create(mock_query_results)
 
@@ -64,9 +67,12 @@ class ExecutorTest(tf.test.TestCase):
     # Mock BigQuery result schema.
     self._schema = [
         bigquery.SchemaField('i', 'INTEGER', mode='REQUIRED'),
+        bigquery.SchemaField('i2', 'INTEGER', mode='REPEATED'),
         bigquery.SchemaField('b', 'BOOLEAN', mode='REQUIRED'),
         bigquery.SchemaField('f', 'FLOAT', mode='REQUIRED'),
+        bigquery.SchemaField('f2', 'FLOAT', mode='REPEATED'),
         bigquery.SchemaField('s', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('s2', 'STRING', mode='REPEATED'),
     ]
     super(ExecutorTest, self).setUp()
 
@@ -86,15 +92,23 @@ class ExecutorTest(tf.test.TestCase):
               exec_properties={
                   '_beam_pipeline_args': ['--project=' + _test_project],
               },
-              split_pattern='SELECT i, b, f, s FROM `fake`'))
+              split_pattern='SELECT i, i2, b, f, f2, s, s2 FROM `fake`'))
 
       feature = {}
       feature['i'] = tf.train.Feature(int64_list=tf.train.Int64List(value=[1]))
+      feature['i2'] = tf.train.Feature(
+          int64_list=tf.train.Int64List(value=[2, 3]))
       feature['b'] = tf.train.Feature(int64_list=tf.train.Int64List(value=[1]))
       feature['f'] = tf.train.Feature(
           float_list=tf.train.FloatList(value=[2.0]))
+      feature['f2'] = tf.train.Feature(
+          float_list=tf.train.FloatList(value=[2.7, 3.8]))
       feature['s'] = tf.train.Feature(
           bytes_list=tf.train.BytesList(value=[tf.compat.as_bytes('abc')]))
+      feature['s2'] = tf.train.Feature(
+          bytes_list=tf.train.BytesList(
+              value=[tf.compat.as_bytes('abc'),
+                     tf.compat.as_bytes('def')]))
       example_proto = tf.train.Example(
           features=tf.train.Features(feature=feature))
       util.assert_that(examples, util.equal_to([example_proto]))
