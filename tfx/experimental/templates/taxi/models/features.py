62ba786f5cdb9a3fe92de6548acb0df718be4866
jiyongjung <jiyongjung@google.com>
2020-05-17 19:49:06 -0700

    Fix list comprehension for bucket features in tfx template.
    PiperOrigin-RevId: 312005513

--- tfx/experimental/templates/taxi/models/features.py
+++ tfx/experimental/templates/taxi/models/features.py
@@ -32,10 +32,10 @@ DENSE_FLOAT_FEATURE_KEYS = ['trip_miles']
 
 # Name of features which have continuous float values. These features will be
 # bucketized using `tft.bucketize`, and will be used as categorical features.
-BUCKET_FEATURE_KEYS = []
+BUCKET_FEATURE_KEYS = ['pickup_latitude']
 # Number of buckets used by tf.transform for encoding each feature. The length
 # of this list should be the same with BUCKET_FEATURE_KEYS.
-BUCKET_FEATURE_BUCKET_COUNT = []
+BUCKET_FEATURE_BUCKET_COUNT = [10]
 
 # Name of features which have categorical values which are mapped to integers.
 # These features will be used as categorical features.
