3e47990acec9ee7977205743295ce80d29244689
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-05-19 18:58:49 -0700

    Fix the bug where tfma is using a nonexistent default metric.
    PiperOrigin-RevId: 312399727

--- tfx/examples/chicago_taxi_pipeline/taxi_pipeline_runtime_parameter.py
+++ tfx/examples/chicago_taxi_pipeline/taxi_pipeline_runtime_parameter.py
@@ -150,7 +150,7 @@ def _create_parameterized_pipeline(
       metrics_specs=[
           tfma.MetricsSpec(
               thresholds={
-                  'binary_accuracy':
+                  'accuracy':
                       tfma.config.MetricThreshold(
                           value_threshold=tfma.GenericValueThreshold(
                               lower_bound={'value': 0.6}),
