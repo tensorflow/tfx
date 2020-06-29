89f0fc20c71cdd157764df5e3b477c7bf9b86b07
tommywei <tommywei@google.com>
2020-06-29 15:46:38 -0700

    more pylint fixes

--- tfx/examples/bert_cola/bert_models.py
+++ tfx/examples/bert_cola/bert_models.py
@@ -23,44 +23,43 @@ def BertForSingleSentenceClassification(
         bert_layer,
         max_len,
         fully_connected_layers=None):
-    """Keras model for single sentence classification.
-    Connect configurable fully connected layers on top of the Bert
-    pooled_output.
+  """Keras model for single sentence classification.
+  Connect configurable fully connected layers on top of the Bert
+  pooled_output.
 
-    Args:
-      bert_layer: A tensroflow_hub.KerasLayer intence of Bert layer.
-      max_len: The maximum length of preprocessed tokens.
-      hidden_layers: List of configurations for fine-tuning hidden layers
-        after the pooled_output. [(#of hidden units, activation)].
+  Args:
+    bert_layer: A tensroflow_hub.KerasLayer intence of Bert layer.
+    max_len: The maximum length of preprocessed tokens.
+    hidden_layers: List of configurations for fine-tuning hidden layers
+      after the pooled_output. [(#of hidden units, activation)].
 
-    Returns:
-      A Keras model.
-    """
-    input_layer_names = [
-        "input_word_ids",
-        "input_mask",
-        "segment_ids"]
+  Returns:
+    A Keras model.
+  """
+  input_layer_names = [
+      "input_word_ids",
+      "input_mask",
+      "segment_ids"]
 
-    input_layers = [
-        keras.layers.Input(
-            shape=(max_len,),
-            dtype=tf.int32,
-            name=name) for name in input_layer_names]
+  input_layers = [
+      keras.layers.Input(
+          shape=(max_len,),
+          dtype=tf.int32,
+          name=name) for name in input_layer_names]
 
-    pooled_output, _ = bert_layer(input_layers)
+  pooled_output, _ = bert_layer(input_layers)
 
-    fully_connected = pooled_output
-    if fully_connected_layers is not None:
-        for (i, activation) in fully_connected_layers:
-            fully_connected = keras.layers.Dense(
-                i,
-                activation=activation)(fully_connected)
+  fully_connected = pooled_output
+  if fully_connected_layers is not None:
+      for (i, activation) in fully_connected_layers:
+          fully_connected = keras.layers.Dense(i,
+              activation=activation)(fully_connected)
 
-    output = keras.layers.Dense(1, activation='sigmoid')(fully_connected)
-    model = keras.Model(input_layers, output)
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(5e-5),
-        loss=tf.keras.losses.binary_crossentropy,
-        metrics=[tfa.metrics.MatthewsCorrelationCoefficient(1)])
+  output = keras.layers.Dense(1, activation='sigmoid')(fully_connected)
+  model = keras.Model(input_layers, output)
+  model.compile(
+      optimizer=tf.keras.optimizers.Adam(5e-5),
+      loss=tf.keras.losses.binary_crossentropy,
+      metrics=[tfa.metrics.MatthewsCorrelationCoefficient(1)])
 
-    return model
+  return model
