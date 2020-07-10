e1909a60df99d23c3f52b8edc2d93c1929c39bb7
muchida <muchida@google.com>
2020-07-09 18:03:52 -0700

    cleanup
    PiperOrigin-RevId: 320513191

--- tfx/extensions/google_cloud_ai_platform/tuner/executor.py
+++ tfx/extensions/google_cloud_ai_platform/tuner/executor.py
@@ -123,8 +123,10 @@ class Executor(base_executor.BaseExecutor):
 
 def _need_chief_oracle(exec_properties: Dict[Text, Any]) -> bool:
   """Returns True if the Tuner instance requires a chief oracle."""
-  # TODO(b/143900133): Add support to CloudTuner that does not require chief
-  #                    oracle process for distributed tuning.
+  # TODO(b/160902662): Skip chief oracle for CloudTuner that does not require
+  #                    chief oracle for distributed tuning (it is a no-op,
+  #                    because it simply forwards  to the AI Platform Optimizer
+  #                    service).
   del exec_properties
   return True
 
