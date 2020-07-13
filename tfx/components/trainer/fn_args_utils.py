298b7d6379fc49cd73016195cfc35d0f0ce10787
Jeffrey Ma <18jeffreyma@gmail.com>
2020-07-13 10:34:05 -0700

    fixing indentation

--- tfx/components/trainer/fn_args_utils.py
+++ tfx/components/trainer/fn_args_utils.py
@@ -60,13 +60,13 @@ def get_common_fn_args(input_dict: Dict[Text, List[types.Artifact]],
   """Get common args of training and tuning."""
   train_files = [
       io_utils.all_files_pattern(uri) for uri in 
-        artifact_utils.get_split_uris(input_dict[constants.EXAMPLES_KEY],
-                                      'train')
+          artifact_utils.get_split_uris(input_dict[constants.EXAMPLES_KEY],
+                                        'train')
   ]
   eval_files = [
       io_utils.all_files_pattern(uri) for uri in 
-        artifact_utils.get_split_uris(input_dict[constants.EXAMPLES_KEY],
-                                      'eval')
+          artifact_utils.get_split_uris(input_dict[constants.EXAMPLES_KEY],
+                                        'eval')
   ]
 
   if input_dict.get(constants.TRANSFORM_GRAPH_KEY):
