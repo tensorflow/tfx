6e9ae195ec3ccfeb1460ab13e307b1cc982c5e8f
Keshav <keshavshivam18@gmail.com>
2020-07-20 12:40:06 -0500

    changed super.__init__()

--- tfx/benchmarks/big_shuffle_benchmark.py
+++ tfx/benchmarks/big_shuffle_benchmark.py
@@ -1,3 +1,16 @@
+# Copyright 2020 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """A BigShuffle workflow."""
 
 from __future__ import absolute_import
@@ -17,7 +30,7 @@ class BigShuffleBenchmarkBase(benchmark_base.BenchmarkBase):
 
   def __init__(self, file_size=1e6, input_file="input.txt",
                output_file="output.txt"):
-    super(BeamPipelineBenchmarkChicagoTaxi, self).__init__()
+    super(BigShuffleBenchmarkBase, self).__init__()
     self.input_file = input_file
     self.output_file = output_file
     self.file_size = file_size # 1e8 bytes = 100 MB
