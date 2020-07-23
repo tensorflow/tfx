2c9568093100e3f5b204938a51d83bf20ca65abf
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-07-23 16:34:30 -0700

    This is an internal feature only.
    PiperOrigin-RevId: 322892446

--- tfx/examples/airflow_workshop/setup/dags/taxi_utils.py
+++ tfx/examples/airflow_workshop/setup/dags/taxi_utils.py
@@ -276,8 +276,7 @@ _FARE_KEY = 'fare'
 #       for colname in _transformed_names(_CATEGORICAL_FEATURE_KEYS)
 #   })
 #
-#   # TODO(b/161952382): Replace with Keras premade models and
-#   # Keras preprocessing layers.
+#   # TODO(b/144500510): SparseFeatures for feature columns + Keras.
 #   deep = tf.keras.layers.DenseFeatures(deep_columns)(input_layers)
 #   for numnodes in dnn_hidden_units:
 #     deep = tf.keras.layers.Dense(numnodes)(deep)
