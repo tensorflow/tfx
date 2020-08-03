5d86d71682cc7dc89054a492f94b1979498a50bd
Jeffrey Ma <18jeffreyma@gmail.com>
2020-08-03 12:01:42 -0700

    version now is not set if None

--- tfx/components/example_gen/driver.py
+++ tfx/components/example_gen/driver.py
@@ -96,9 +96,10 @@ class Driver(base_driver.BaseDriver):
     example_artifact.set_string_custom_property(
         utils.SPAN_PROPERTY_NAME, exec_properties[utils.SPAN_PROPERTY_NAME])
     # TODO(b/162622803): add default behavior for when version spec not present.
+    if exec_properties[utils.VERSION_PROPERTY_NAME]:
     example_artifact.set_string_custom_property(
         utils.VERSION_PROPERTY_NAME,
-        exec_properties[utils.VERSION_PROPERTY_NAME] or '')
+        exec_properties[utils.VERSION_PROPERTY_NAME])
 
     base_driver._prepare_output_paths(example_artifact)  # pylint: disable=protected-access
 
