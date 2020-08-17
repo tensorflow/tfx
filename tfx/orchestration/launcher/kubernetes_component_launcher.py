c90450bd6509f154775c6693841969a697a5a698
Eric-Le-Ge <ericlege@google.com>
2020-08-17 12:32:04 -0700

    remove unused import

--- tfx/orchestration/launcher/kubernetes_component_launcher.py
+++ tfx/orchestration/launcher/kubernetes_component_launcher.py
@@ -19,7 +19,6 @@ from __future__ import division
 from __future__ import print_function
 
 import datetime
-import re
 import time
 from typing import Any, Callable, Dict, List, Optional, Text, cast
 
