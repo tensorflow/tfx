f805f115abbc3ec079c0f2f23859d75a288b0e17
Gerard Casas Saez <gcasassaez@twitter.com>
2020-08-28 18:50:06 -0600

    format back to 2 spaces

--- tfx/experimental/metadata_client/client_test.py
+++ tfx/experimental/metadata_client/client_test.py
@@ -1,3 +1,4 @@
+"""Tests for tfx.experimental.metadata_client.client."""
 import os
 
 import tensorflow as tf
@@ -9,59 +10,61 @@ from tfx.orchestration.pipeline import Pipeline
 from tfx.utils.dsl_utils import external_input
 
 
-def create_pipeline(root_dir: str):
-    root_path = os.path.join(root_dir, "tfx")
-    csv_content = ["a,b,c,d", "1,s,3,d"]
-    with open(os.path.join(root_path, "csv", "test.csv"), "w+") as f:
-        f.writelines(csv_content)
-    example_gen = CsvExampleGen(input=external_input(os.path.join(root_path, "csv")))
-    connection_config = metadata_store_pb2.ConnectionConfig()
-    connection_config.sqlite.filename_uri = os.path.join(root_path, "db.sqlite")
-    connection_config.sqlite.connection_mode = 3  # READWRITE_OPENCREATE
-    return Pipeline(
-        pipeline_root=root_path,
-        pipeline_name="client_test",
-        metadata_connection_config=connection_config,
-        components=[example_gen],
-    )
+def _create_pipeline(root_dir: str):
+  root_path = os.path.join(root_dir, "tfx")
+  csv_content = ["a,b,c,d", "1,s,3,d"]
+  with open(os.path.join(root_path, "csv", "test.csv"), "w+") as f:
+    f.writelines(csv_content)
+  example_gen = CsvExampleGen(
+    input=external_input(os.path.join(root_path, "csv")))
+  connection_config = metadata_store_pb2.ConnectionConfig()
+  connection_config.sqlite.filename_uri = os.path.join(root_path, "db.sqlite")
+  connection_config.sqlite.connection_mode = 3  # READWRITE_OPENCREATE
+  return Pipeline(
+    pipeline_root=root_path,
+    pipeline_name="client_test",
+    metadata_connection_config=connection_config,
+    components=[example_gen],
+  )
 
 
-class MLMDClientTestCase(tf.test.TestCase):
-    def setUp(self):
-        pipeline = create_pipeline(self.get_temp_dir())
-        self.connection_config = pipeline.metadata_connection_config
-        self.pipeline_name = pipeline.pipeline_info.pipeline_name
-        self.pipeline = pipeline
-        super().setUp()
+class MetadataClientTestCase(tf.test.TestCase):
+  """MetadataClient tests
+  """
 
-    def test_pipeline_exists(self):
-        BeamDagRunner().run(self.pipeline)
-        p = client.MetadataClient.from_pipeline(self.pipeline)
-        self.assertIsInstance(p, client.PipelineContext)
+  def setUp(self):
+    pipeline = _create_pipeline(self.get_temp_dir())
+    self.connection_config = pipeline.metadata_connection_config
+    self.pipeline_name = pipeline.pipeline_info.pipeline_name
+    self.pipeline = pipeline
+    super().setUp()
 
-    def test_pipeline_run_exists(self):
-        BeamDagRunner().run(self.pipeline)
-        runs = client.MetadataClient.from_pipeline(self.pipeline).runs
-        self.assertEqual(len(runs), 1)
+  def test_pipeline_exists(self):
+    BeamDagRunner().run(self.pipeline)
+    p = client.MetadataClient.from_pipeline(self.pipeline)
+    self.assertIsInstance(p, client.PipelineContext)
 
-    def test_get_status(self):
-        BeamDagRunner().run(self.pipeline)
-        run = (
-            client.MetadataClient(self.connection_config)
-            .get_pipeline(self.pipeline_name)
-            .runs[0]
-        )
-        self.assertDictEqual(run.get_status(), {"CsvExampleGen": "complete"})
+  def test_pipeline_run_exists(self):
+    BeamDagRunner().run(self.pipeline)
+    runs = client.MetadataClient.from_pipeline(self.pipeline).runs
+    self.assertEqual(len(runs), 1)
 
-    def test_pipeline_context_attributes(self):
-        BeamDagRunner().run(self.pipeline)
-        pipeline = client.MetadataClient.from_pipeline(self.pipeline)
-        self.assertEqual(pipeline.name, self.pipeline_name)
-        self.assertEqual(pipeline.runs[0].pipeline_name, self.pipeline_name)
+  def test_get_status(self):
+    BeamDagRunner().run(self.pipeline)
+    run = (client.MetadataClient(self.connection_config).get_pipeline(
+      self.pipeline_name).runs[0])
+    self.assertDictEqual(run.get_status(), {"CsvExampleGen": "complete"})
 
-    def test_get_pipeline_not_run(self):
-        pipeline = client.MetadataClient.from_pipeline(self.pipeline)
-        self.assertIsNone(pipeline)
+  def test_pipeline_context_attributes(self):
+    BeamDagRunner().run(self.pipeline)
+    pipeline = client.MetadataClient.from_pipeline(self.pipeline)
+    self.assertEqual(pipeline.name, self.pipeline_name)
+    self.assertEqual(pipeline.runs[0].pipeline_name, self.pipeline_name)
+
+  def test_get_pipeline_not_run(self):
+    pipeline = client.MetadataClient.from_pipeline(self.pipeline)
+    self.assertIsNone(pipeline)
+
 
 if __name__ == '__main__':
   tf.test.main()
