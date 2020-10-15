45f31dedb342d191c39b00603a0d1e6df21e4744
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-10-15 16:05:57 -0700

    noop
    PiperOrigin-RevId: 337402022

--- tfx/dsl/compiler/compiler.py
+++ tfx/dsl/compiler/compiler.py
@@ -17,6 +17,8 @@ import re
 
 from typing import cast
 
+from absl import logging
+
 from tfx.components.common_nodes import importer_node
 from tfx.components.common_nodes import resolver_node
 from tfx.dsl.compiler import compiler_utils
@@ -171,8 +173,10 @@ class Compiler(object):
         node.inputs.resolver_config.resolver_policy = (
             pipeline_pb2.ResolverConfig.ResolverPolicy.LATEST_BLESSED_MODEL)
       else:
-        raise ValueError("Got unsupported resolver policy: {}".format(
-            resolver.type))
+        logging.error("Got unsupported resolver policy: %s", type(resolver))
+        node.inputs.resolver_config.resolver_policy = (
+            pipeline_pb2.ResolverConfig.ResolverPolicy
+            .RESOLVER_POLICY_UNSPECIFIED)
 
     # Step 4: Node outputs
     if isinstance(tfx_node, base_component.BaseComponent):
