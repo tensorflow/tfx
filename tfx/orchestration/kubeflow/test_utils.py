afc452415531a74ecca402502ab025fe812238b2
zhitaoli <zhitaoli@google.com>
2020-10-26 15:59:30 -0700

    Invisible
    PiperOrigin-RevId: 339137111

--- tfx/orchestration/kubeflow/test_utils.py
+++ tfx/orchestration/kubeflow/test_utils.py
@@ -621,6 +621,13 @@ class BaseKubeflowTest(tf.test.TestCase):
         '--temp_location=' +
         os.path.join(self._pipeline_root(pipeline_name), 'tmp'),
         '--region=' + self._GCP_REGION,
+
+        # TODO(b/171733562): Remove this once runner v2 is the default for
+        # Dataflow.
+        '--experiments=use_runner_v2',
+        # TODO(b/171733562): Remove this once type check is restored with
+        # beam-2717.
+        '--no_pipeline_type_check',
     ]
     return pipeline
 
