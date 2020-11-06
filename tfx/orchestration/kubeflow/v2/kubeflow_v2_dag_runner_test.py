1bd4f2fcbfc6fedc9d986ccbc0887932f74a6eb1
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-11-06 10:06:27 -0800

    Fix the SDK to docker version transformation.
    PiperOrigin-RevId: 341069956

--- tfx/orchestration/kubeflow/v2/kubeflow_v2_dag_runner_test.py
+++ tfx/orchestration/kubeflow/v2/kubeflow_v2_dag_runner_test.py
@@ -37,6 +37,16 @@ _TEST_FILE_NAME = 'test_pipeline_1.json'
 _ILLEGALLY_NAMED_PIPELINE = tfx_pipeline.Pipeline(
     pipeline_name='ThisIsIllegal', pipeline_root='/some/path', components=[])
 
+# Sample versions used for testing SDK to docker version transformation.
+_SAMPLE_RELEASED_VERSION = '0.25.0'
+_SAMPLE_RC_VERSION = '0.25.0-rc1'
+_SAMPLE_NIGHTLY_VERSION = '0.25.0.dev20201101'
+_SAMPLE_DEV_VERSION = '0.26.0.dev'
+_EXPECTED_RELEASED_DOCKER_VERSION = '0.25.0'
+_EXPECTED_RC_DOCKER_VERSION = '0.25.0rc1'
+_EXPECTED_NIGHTLY_DOCKER_VERSION = '0.25.0.dev20201101'
+_EXPECTED_DEV_DOCKER_VERSION = 'latest'
+
 
 class KubeflowV2DagRunnerTest(tf.test.TestCase):
 
@@ -50,8 +60,7 @@ class KubeflowV2DagRunnerTest(tf.test.TestCase):
     shutil.rmtree(self.test_dir)
 
   def _compare_against_testdata(
-      self,
-      runner: kubeflow_v2_dag_runner.KubeflowV2DagRunner,
+      self, runner: kubeflow_v2_dag_runner.KubeflowV2DagRunner,
       pipeline: tfx_pipeline.Pipeline, golden_file: Text):
     """Compiles and compare the actual JSON output against a golden file."""
     actual_output = runner.run(pipeline=pipeline, write_out=True)
@@ -69,10 +78,23 @@ class KubeflowV2DagRunnerTest(tf.test.TestCase):
 
     self.assertDictEqual(actual_json, expected_json)
 
+  def testImageVersionFormatting(self):
+    self.assertEqual(
+        kubeflow_v2_dag_runner.get_image_version(_SAMPLE_RELEASED_VERSION),
+        _EXPECTED_RELEASED_DOCKER_VERSION)
+    self.assertEqual(
+        kubeflow_v2_dag_runner.get_image_version(_SAMPLE_RC_VERSION),
+        _EXPECTED_RC_DOCKER_VERSION)
+    self.assertEqual(
+        kubeflow_v2_dag_runner.get_image_version(_SAMPLE_NIGHTLY_VERSION),
+        _EXPECTED_NIGHTLY_DOCKER_VERSION)
+    self.assertEqual(
+        kubeflow_v2_dag_runner.get_image_version(_SAMPLE_DEV_VERSION),
+        _EXPECTED_DEV_DOCKER_VERSION)
+
   @mock.patch('sys.version_info')
   @mock.patch(
-      'tfx.orchestration.kubeflow.v2.kubeflow_v2_dag_runner._get_current_time'
-  )
+      'tfx.orchestration.kubeflow.v2.kubeflow_v2_dag_runner._get_current_time')
   def testCompileTwoStepPipeline(self, fake_now, fake_sys_version):
     fake_now.return_value = datetime.date(2020, 1, 1)
     fake_sys_version.major = 3
@@ -80,8 +102,7 @@ class KubeflowV2DagRunnerTest(tf.test.TestCase):
     runner = kubeflow_v2_dag_runner.KubeflowV2DagRunner(
         output_dir=_TEST_DIR,
         output_filename=_TEST_FILE_NAME,
-        config=kubeflow_v2_dag_runner
-        .KubeflowV2DagRunnerConfig(
+        config=kubeflow_v2_dag_runner.KubeflowV2DagRunnerConfig(
             project_id='my-project',
             display_name='my-pipeline',
             default_image='gcr.io/my-tfx:latest'))
@@ -93,8 +114,7 @@ class KubeflowV2DagRunnerTest(tf.test.TestCase):
 
   @mock.patch('sys.version_info')
   @mock.patch(
-      'tfx.orchestration.kubeflow.v2.kubeflow_v2_dag_runner._get_current_time'
-  )
+      'tfx.orchestration.kubeflow.v2.kubeflow_v2_dag_runner._get_current_time')
   def testCompileFullTaxiPipeline(self, fake_now, fake_sys_version):
     fake_now.return_value = datetime.date(2020, 1, 1)
     fake_sys_version.major = 3
@@ -102,8 +122,7 @@ class KubeflowV2DagRunnerTest(tf.test.TestCase):
     runner = kubeflow_v2_dag_runner.KubeflowV2DagRunner(
         output_dir=_TEST_DIR,
         output_filename=_TEST_FILE_NAME,
-        config=kubeflow_v2_dag_runner
-        .KubeflowV2DagRunnerConfig(
+        config=kubeflow_v2_dag_runner.KubeflowV2DagRunnerConfig(
             project_id='my-project',
             display_name='my-pipeline',
             default_image='tensorflow/tfx:latest'))
