85d32bfcbf959ced5f4f7c3fa32f9df7ce7e4e54
yoonmiroh <yoonmiroh@google.com>
2021-03-21 20:13:11 -0700

    Internal Change
    PiperOrigin-RevId: 364230119

--- tfx/orchestration/kubeflow/v2/step_builder.py
+++ tfx/orchestration/kubeflow/v2/step_builder.py
@@ -376,18 +376,17 @@ class StepBuilder(object):
   def _build_importer_spec(self) -> ImporterSpec:
     """Builds ImporterSpec."""
     assert isinstance(self._node, importer.Importer)
-    output_channel = self._node.outputs[importer.IMPORT_RESULT_KEY]
     result = ImporterSpec(
         properties=compiler_utils.convert_from_tfx_properties(
-            output_channel.additional_properties),
+            self._exec_properties[importer.PROPERTIES_KEY]),
         custom_properties=compiler_utils.convert_from_tfx_properties(
-            output_channel.additional_custom_properties))
+            self._exec_properties[importer.CUSTOM_PROPERTIES_KEY]))
     result.reimport = bool(self._exec_properties[importer.REIMPORT_OPTION_KEY])
     result.artifact_uri.CopyFrom(
         compiler_utils.value_converter(
             self._exec_properties[importer.SOURCE_URI_KEY]))
     single_artifact = artifact_utils.get_single_instance(
-        list(output_channel.get()))
+        list(self._node.outputs[importer.IMPORT_RESULT_KEY].get()))
     result.type_schema.CopyFrom(
         pipeline_pb2.ArtifactTypeSchema(
             instance_schema=compiler_utils.get_artifact_schema(
