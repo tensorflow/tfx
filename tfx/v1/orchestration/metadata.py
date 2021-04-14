6c4291f8d17b748baca72e44a1d4420349934fe5
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2021-04-14 15:42:14 -0700

    Marked public API for metadata.py
    PiperOrigin-RevId: 368523165

--- tfx/v1/orchestration/metadata.py
+++ tfx/v1/orchestration/metadata.py
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""TFX orchestration module."""
+"""Public API for metadata."""
 
-from tfx.v1.orchestration import metadata
+from tfx.orchestration import metadata
+
+ConnectionConfigType = metadata.ConnectionConfigType
+mysql_metadata_connection_config = metadata.mysql_metadata_connection_config
+sqlite_metadata_connection_config = metadata.sqlite_metadata_connection_config
