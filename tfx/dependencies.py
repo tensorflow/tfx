e0fac271e3dcf06973aed87739a10af5f980be48
dhruvesh09 <30351549+dhruvesh09@users.noreply.github.com>
2021-05-17 09:54:03 -0700

    Update Beam constraints (#3753)

--- tfx/dependencies.py
+++ tfx/dependencies.py
@@ -71,7 +71,7 @@ def make_required_install_packages():
   # Make sure to sync the versions of common dependencies (absl-py, numpy,
   # six, and protobuf) with TF.
   return make_pipeline_sdk_required_install_packages() + [
-      'apache-beam[gcp]>=2.29,<3',
+      'apache-beam[gcp]>=2.28,<3',
       'attrs>=19.3.0,<21',
       'click>=7,<8',
       'google-api-python-client>=1.7.8,<2',
