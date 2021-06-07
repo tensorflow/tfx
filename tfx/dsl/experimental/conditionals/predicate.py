c645dd3f221337b5dcb9f988e63c747f72f3f644
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2021-06-07 16:31:43 -0700

    Skeleton interface between Predicate and compiler.
    PiperOrigin-RevId: 378032619

--- tfx/dsl/experimental/conditionals/predicate.py
+++ tfx/dsl/experimental/conditionals/predicate.py
@@ -13,20 +13,66 @@
 # limitations under the License.
 """Predicate for Conditional channels."""
 
-from typing import Any, Dict
+from typing import Any, Dict, Callable
 from tfx.utils import json_utils
+
+# Not importing tfx.types, because it will cause circular dependencies later on.
+# tfx.types -> predicate -> placeholder -> channels -> tfx.types
+tfx_types = Any
+
+
+class PredicateWithInputKeys:
+
+  def to_json_dict(self) -> Dict[str, Any]:
+    return {}
 
 
 class Predicate(json_utils.Jsonable):
   """Experimental Predicate object."""
 
-  def to_json_dict(self) -> Dict[str, Any]:
-    return {}
+  def replace_placeholder_keys(
+      self, channel_to_key_fn: Callable[['tfx_types.Channel'], str]
+  ) -> PredicateWithInputKeys:
+    """Replaces the placeholder key, enabling Predicate resolution.
 
-  @classmethod
-  def from_json_dict(cls, dict_data: Dict[str, Any]) -> 'Predicate':
-    return Predicate()
+    Predicate objects are created during Pipeline authoring time, composed from
+    output Channels of upstream nodes, whose future values are represented by
+    ArtifactPlaceholders. These values are available at input resolution time,
+    and at that time, they can be retrieved from a dictionary by supplying the
+    right input channel key.
 
-  def negated(self):
-    """Applies a NOT operation to the Predicate."""
-    return Predicate()
+    However:
+    1) Different nodes that map to the same Predicate might use different input
+       keys to refer to the same underlying Channel.
+    2) When a Predicate object is first constructed, it doesn't know which nodes
+       are going to use it.
+
+    Thus, the ArtifactPlaceholders that represent the Channels' future values
+    are initially constructed without any "key". The "keys" will be "filled in"
+    only at compilation time -- when the compiler is converting the DSL Node
+    object into IR, the Predicate objects for that node will have their
+    underlying ArtifactPlaceholders be filled in with their respective input
+    keys. Consequently, each node's IR will contain the right input key for
+    obtain the Channel's future value, enabling it to resolve the Predicate.
+
+    To do so, the compiler implements a function `channel_to_key_fn`, which maps
+    a Channel to the input key for retrieving the future values at input
+    resolution time.
+
+    When `replace_placeholder_keys` method is called on a Predicate
+    instance with the `channel_to_key_fn`, it will traverse its internal data
+    structure, and whenever it finds a ArtifactPlaceholder, it will retrieve the
+    associated Channel, call `channel_to_key_fn` with it, and use the returned
+    key to replace the ArtifactPlaceholder's key.
+
+    Args:
+      channel_to_key_fn: A function that takes a Channel and produces the input
+        key. Note that the channel_to_key_fn for different nodes would likely be
+        different, since different nodes can use different input keys to refer
+        to the same Channel during input resolution.
+
+    Returns:
+      A copy of the Predicate, with the ArtifactPlaceholders' keys filled in.
+    """
+    del channel_to_key_fn
+    return PredicateWithInputKeys()
