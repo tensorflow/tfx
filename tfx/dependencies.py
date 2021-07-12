0b808194d73a58b6138c9acafd165ef3557cfb15
jiyongjung <jiyongjung@google.com>
2021-07-11 21:51:29 -0700

    Blocks installation of jax 0.2.17 which is incompatible with current example.
    PiperOrigin-RevId: 384163140

--- tfx/dependencies.py
+++ tfx/dependencies.py
@@ -189,7 +189,8 @@ def make_extra_packages_examples():
       #     examples/penguin/experimental/penguin_pipeline_sklearn_gcp.py)
       # Required for the experimental tfx/examples using Flax, e.g.,
       # tfx/examples/penguin.
-      'jax>=0.2.13,<0.3',
+      # TODO(b/193362300): Unblock the version cap after fixing the example.
+      'jax>=0.2.13,<0.2.17',
       'jaxlib>=0.1.64,<0.2',
       'flax>=0.3.3,<0.4',
       # Required for tfx/examples/penguin/penguin_utils_cloud_tuner.py
