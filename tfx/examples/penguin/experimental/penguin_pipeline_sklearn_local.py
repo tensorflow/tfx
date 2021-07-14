c373b5f5acbfd900e779368bac2b5eb4c5ad7a01
dhruvesh09 <30351549+dhruvesh09@users.noreply.github.com>
2021-07-13 17:01:11 -0700

    Add BI component to penguin local and removed taxi inference example (#4035)
    PiperOrigin-RevId: 375608679
    
    Co-authored-by: tfx-team <tensorflow-extended-nonhuman@googlegroups.com>

--- tfx/examples/penguin/experimental/penguin_pipeline_sklearn_local.py
+++ tfx/examples/penguin/experimental/penguin_pipeline_sklearn_local.py
@@ -85,7 +85,8 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
                      beam_pipeline_args: List[Text]) -> pipeline.Pipeline:
   """Implements the Penguin pipeline with TFX."""
   # Brings data into the pipeline or otherwise joins/converts training data.
-  example_gen = CsvExampleGen(input_base=data_root)
+  example_gen = tfx.components.CsvExampleGen(
+      input_base=os.path.join(data_root, 'labelled'))
 
   # Computes statistics over data for visualization and example validation.
   statistics_gen = StatisticsGen(examples=example_gen.outputs['examples'])
