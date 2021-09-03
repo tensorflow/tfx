411b4e5c8719bd5f25152dbcae85dabbbedb2d7c
swap-10 <swapnalv10@gmail.com>
2021-09-03 19:17:01 +0530

    Add dataset source acknowledgement

--- tfx/examples/imdb/imdb_fetch_data.py
+++ tfx/examples/imdb/imdb_fetch_data.py
@@ -8,6 +8,21 @@ import tensorflow_datasets as tfds
 # full_data.fetch_data()
 
 
+"""
+@InProceedings{maas-EtAl:2011:ACL-HLT2011,
+  author    = {Maas, Andrew L.  and  Daly, Raymond E.  and  Pham, Peter T.  and  Huang, Dan  and  Ng, Andrew Y.  and  Potts, Christopher},
+  title     = {Learning Word Vectors for Sentiment Analysis},
+  booktitle = {Proceedings of the 49th Annual Meeting of the Association for Computational Linguistics: Human Language Technologies},
+  month     = {June},
+  year      = {2011},
+  address   = {Portland, Oregon, USA},
+  publisher = {Association for Computational Linguistics},
+  pages     = {142--150},
+  url       = {http://www.aclweb.org/anthology/P11-1015}
+}
+"""
+
+
 def fetch_data():
   """This downloads the full dataset to pwd/data/imdb.csv"""
 
