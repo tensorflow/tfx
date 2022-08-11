6429c643233f1c1fca41c7c02e7da966c763c7eb
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2022-08-11 15:42:02 -0700

    Add `StaticRange`, a `ResolverOp` that returns artifacts with spans in [start_span, end_span] inclusive.
    PiperOrigin-RevId: 467067187

--- tfx/dsl/input_resolution/ops/ops.py
+++ tfx/dsl/input_resolution/ops/ops.py
@@ -20,6 +20,7 @@ from tfx.dsl.input_resolution import resolver_op
 from tfx.dsl.input_resolution.ops import latest_create_time_op
 from tfx.dsl.input_resolution.ops import latest_span_op
 from tfx.dsl.input_resolution.ops import skip_if_empty_op
+from tfx.dsl.input_resolution.ops import static_span_range_op
 from tfx.dsl.input_resolution.ops import unnest_op
 from tfx.dsl.input_resolution.strategies import conditional_strategy
 from tfx.dsl.input_resolution.strategies import latest_artifact_strategy
@@ -48,12 +49,14 @@ def _register_op(cls: _ResolverOpType) -> None:
 LatestCreateTime = latest_create_time_op.LatestCreateTime
 LatestSpan = latest_span_op.LatestSpan
 SkipIfEmpty = skip_if_empty_op.SkipIfEmpty
+StaticSpanRange = static_span_range_op.StaticSpanRange
 Unnest = unnest_op.Unnest
 # go/keep-sorted end
 # go/keep-sorted start
 _register_op(LatestCreateTime)
 _register_op(LatestSpan)
 _register_op(SkipIfEmpty)
+_register_op(StaticSpanRange)
 _register_op(Unnest)
 # go/keep-sorted end
 
