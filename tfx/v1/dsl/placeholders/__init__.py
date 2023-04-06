578133525c97928b750fba6a0037fa71b75be327
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2023-04-06 07:13:45 -0700

    No op.
    PiperOrigin-RevId: 522328404

--- tfx/v1/dsl/placeholders/__init__.py
+++ tfx/v1/dsl/placeholders/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """TFX placeholders module."""
+# pylint: disable=unused-import
 
 from tfx.dsl.placeholder.placeholder import exec_property
 from tfx.dsl.placeholder.placeholder import input  # pylint: disable=redefined-builtin
