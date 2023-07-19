a9491388ce0522a032f18179ec9214520657a649
anuartb <anuartb@google.com>
2023-07-18 22:30:48 -0700

    Pin pyyaml version to 5.3.1, to fix the build error.
    PiperOrigin-RevId: 549206552

--- RELEASE.md
+++ RELEASE.md
@@ -36,6 +36,7 @@
 | `google-api-core` | `<2` | `<1.33` | |
 | `google-cloud-aiplatform` | `>=1.6.2,<2` | `>=1.6.2,<1.18` | |
 | `tflite-support` | `>=0.4.3,<0.4.5` | `>=0.4.2,<0.4.3` | |
+| `pyyaml` | `==5.3.1`| `>=3.12,<6` | Issue with installation of PyYaml 5.4.1. (https://github.com/yaml/pyyaml/issues/724) |
 
 ## Documentation Updates
 
--- tfx/dependencies.py
+++ tfx/dependencies.py
@@ -88,7 +88,9 @@ def make_required_install_packages():
       'kubernetes>=10.0.1,<13',
       'numpy>=1.16,<2',
       'pyarrow>=10,<11',
-      'pyyaml>=3.12,<6',
+      # TODO(b/291837844): Pinned pyyaml to 5.3.1.
+      # Unpin once the issue with installation is resolved.
+      'pyyaml>=3.12,<6,!=5.4.0,!=5.4.1',
       # Keep the TF version same as TFT to help Pip version resolution.
       # Pip might stuck in a TF 1.15 dependency although there is a working
       # dependency set with TF 2.x without the sync.
