f05d1868a62accd5d36c1acef8381a03b7df3283
anuartb <anuartb@google.com>
2023-08-23 23:21:15 -0700

    no-op
    PiperOrigin-RevId: 559654755

--- tfx/dependencies.py
+++ tfx/dependencies.py
@@ -195,7 +195,7 @@ def make_extra_packages_tfdf():
   # Required for tfx/examples/penguin/penguin_utils_tfdf_experimental.py
   return [
       # NOTE: TFDF 1.0.1 is only compatible with TF 2.10.x.
-      'tensorflow-decision-forests>=1.0.1,<2',
+      'tensorflow-decision-forests>=1.0.1,<2,^1.6.0rc0',
   ]
 
 
