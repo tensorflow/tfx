47e7407030a1cadc9d0eae1765be45456d4c4c57
anuartb <anuartb@google.com>
2023-08-30 00:21:38 -0700

    no-op
    PiperOrigin-RevId: 561254105

--- tfx/dependencies.py
+++ tfx/dependencies.py
@@ -86,7 +86,7 @@ def make_required_install_packages():
       'grpcio>=1.28.1,<2',
       'keras-tuner>=1.0.4,<2',
       'kubernetes>=10.0.1,<13',
-      'numpy>=1.16,<2',
+      'numpy>=1.17,<2',
       'pyarrow>=10,<11',
       # TODO(b/291837844): Pinned pyyaml to 5.3.1.
       # Unpin once the issue with installation is resolved.
