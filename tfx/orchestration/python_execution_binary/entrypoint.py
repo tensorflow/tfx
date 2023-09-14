2edabaf278af70ff83bb0e0d301be7090ed0a83a
wssong <wssong@google.com>
2023-09-14 05:46:05 -0700

    No op
    PiperOrigin-RevId: 565342437

--- tfx/orchestration/python_execution_binary/entrypoint.py
+++ tfx/orchestration/python_execution_binary/entrypoint.py
@@ -17,20 +17,12 @@ This library is intended to serve as the entrypoint for a binary that packages
 the python executors in a pipeline. The resulting binary is called by the TFX
 launcher and should not be called directly.
 """
-from typing import Union, cast
 
 from absl import flags
 from absl import logging
-from tfx.dsl.io import fileio
-from tfx.orchestration import metadata
-from tfx.orchestration.portable import data_types
-from tfx.orchestration.portable import python_driver_operator
 from tfx.orchestration.python_execution_binary import python_execution_binary_utils
-from tfx.proto.orchestration import driver_output_pb2
-from tfx.proto.orchestration import executable_spec_pb2
-from tfx.utils import import_utils
+from tfx.orchestration.python_execution_binary import python_execution_lib
 
-from tfx.orchestration.python_execution_binary import python_executor_operator_dispatcher
 from google.protobuf import text_format
 
 FLAGS = flags.FLAGS
@@ -50,68 +42,37 @@ MLMD_CONNECTION_CONFIG_FLAG = flags.DEFINE_string(
     'indicates a driver execution')
 
 
-def _import_class_path(
-    executable_spec: Union[executable_spec_pb2.PythonClassExecutableSpec,
-                           executable_spec_pb2.BeamExecutableSpec],):
-  """Import the class path from Python or Beam executor spec."""
-  if isinstance(executable_spec, executable_spec_pb2.BeamExecutableSpec):
-    beam_executor_spec = cast(executable_spec_pb2.BeamExecutableSpec,
-                              executable_spec)
-    import_utils.import_class_by_path(
-        beam_executor_spec.python_executor_spec.class_path)
-  else:
-    python_class_executor_spec = cast(
-        executable_spec_pb2.PythonClassExecutableSpec, executable_spec)
-    import_utils.import_class_by_path(python_class_executor_spec.class_path)
-
-
-def _run_driver(
-    executable_spec: Union[executable_spec_pb2.PythonClassExecutableSpec,
-                           executable_spec_pb2.BeamExecutableSpec],
-    mlmd_connection_config: metadata.ConnectionConfigType,
-    execution_info: data_types.ExecutionInfo) -> driver_output_pb2.DriverOutput:
-  operator = python_driver_operator.PythonDriverOperator(
-      executable_spec, metadata.Metadata(mlmd_connection_config))
-  return operator.run_driver(execution_info)
-
-
 def main(_):
   flags.mark_flag_as_required(EXECUTION_INVOCATION_FLAG.name)
   flags.mark_flags_as_mutual_exclusive(
       (EXECUTABLE_SPEC_FLAG.name, BEAM_EXECUTABLE_SPEC_FLAG.name),
       required=True)
 
-  deserialized_executable_spec = None
   if BEAM_EXECUTABLE_SPEC_FLAG.value is not None:
-    deserialized_executable_spec = (
-        python_execution_binary_utils.deserialize_executable_spec(
-            BEAM_EXECUTABLE_SPEC_FLAG.value, with_beam=True))
+    executable_spec = python_execution_binary_utils.deserialize_executable_spec(
+        BEAM_EXECUTABLE_SPEC_FLAG.value, with_beam=True
+    )
   else:
-    deserialized_executable_spec = (
-        python_execution_binary_utils.deserialize_executable_spec(
-            EXECUTABLE_SPEC_FLAG.value, with_beam=False))
-  # Eagerly import class path from executable spec such that all artifact
-  # references are resolved.
-  _import_class_path(deserialized_executable_spec)
+    executable_spec = python_execution_binary_utils.deserialize_executable_spec(
+        EXECUTABLE_SPEC_FLAG.value, with_beam=False
+    )
   execution_info = python_execution_binary_utils.deserialize_execution_info(
-      EXECUTION_INVOCATION_FLAG.value)
+      EXECUTION_INVOCATION_FLAG.value
+  )
   logging.info('execution_info = %r\n', execution_info)
-  logging.info('executable_spec = %s\n',
-               text_format.MessageToString(deserialized_executable_spec))
+  logging.info(
+      'executable_spec = %s\n', text_format.MessageToString(executable_spec)
+  )
 
-  # MLMD connection config being set indicates a driver execution instead of an
-  # executor execution as accessing MLMD is not supported for executors.
   if MLMD_CONNECTION_CONFIG_FLAG.value:
     mlmd_connection_config = (
         python_execution_binary_utils.deserialize_mlmd_connection_config(
-            MLMD_CONNECTION_CONFIG_FLAG.value))
-    run_result = _run_driver(deserialized_executable_spec,
-                             mlmd_connection_config, execution_info)
-  else:
-    run_result = python_executor_operator_dispatcher.run_executor(
-        deserialized_executable_spec, execution_info
+            MLMD_CONNECTION_CONFIG_FLAG.value
+        )
     )
+  else:
+    mlmd_connection_config = None
 
-  if run_result:
-    with fileio.open(execution_info.execution_output_uri, 'wb') as f:
-      f.write(run_result.SerializeToString())
+  python_execution_lib.run(
+      executable_spec, execution_info, mlmd_connection_config
+  )
