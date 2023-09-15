73c528f4d7886a0c77ef7fb3d27ba6b3da2c71ca
kmonte <kmonte@google.com>
2023-09-15 14:02:15 -0700

    Fix bug where map is lazily evaluated
    PiperOrigin-RevId: 565772130

--- tfx/orchestration/experimental/core/async_pipeline_task_gen.py
+++ tfx/orchestration/experimental/core/async_pipeline_task_gen.py
@@ -411,7 +411,8 @@ class _Generator:
         input_and_params=unprocessed_inputs,
     )
 
-    map(lambda e: execution_state_change_fn(None, e), executions)
+    for execution in executions:
+      execution_state_change_fn(None, execution)
 
     result.extend(
         task_gen_utils.generate_tasks_from_one_input(
