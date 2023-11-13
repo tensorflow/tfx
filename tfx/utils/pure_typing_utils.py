7f5def579c4c42b9e385a663ad7ac2c43910999d
jjong <jjong@google.com>
2023-11-12 21:36:02 -0800

    Add maybe_unwrap_optional.
    PiperOrigin-RevId: 581838673

--- tfx/utils/pure_typing_utils.py
+++ tfx/utils/pure_typing_utils.py
@@ -32,6 +32,11 @@ from google.protobuf.internal import enum_type_wrapper
 _T = TypeVar('_T')
 _TTypedDict = TypeVar('_TTypedDict', bound=TypedDict)
 
+if sys.version_info >= (3, 10):
+  _UNION_ORIGINS = (types.UnionType, typing.Union)
+else:
+  _UNION_ORIGINS = (typing.Union,)
+
 
 # This is intentionally not public as it aims for the annotations resolver only
 # for `is_compatible`.
@@ -97,10 +102,8 @@ def is_compatible(value: Any, tp: Type[_T]) -> TypeGuard[_T]:
       return isinstance(value, tp)
   if maybe_origin is not None:
     # Union[T, U] or T | U
-    if maybe_origin is typing.Union:
+    if maybe_origin in _UNION_ORIGINS:
       assert maybe_args, f'{tp} should be subscripted.'
-      return any(is_compatible(value, arg) for arg in maybe_args)
-    if sys.version_info >= (3, 10) and maybe_origin is types.UnionType:
       return any(is_compatible(value, arg) for arg in maybe_args)
     # Type[T]
     elif maybe_origin is type:
@@ -183,3 +186,13 @@ def is_compatible(value: Any, tp: Type[_T]) -> TypeGuard[_T]:
           f'Type {tp} with unsupported origin type {maybe_origin}.'
       )
   raise NotImplementedError(f'Unsupported type {tp}.')
+
+
+def maybe_unwrap_optional(type_hint: Any) -> tuple[bool, Any]:
+  origin = typing.get_origin(type_hint)
+  args = typing.get_args(type_hint)
+  if origin and origin in _UNION_ORIGINS and args and type(None) in args:
+    args_except_none = [t for t in args if t != type(None)]
+    if len(args_except_none) == 1:
+      return True, args_except_none[0]
+  return False, type_hint
