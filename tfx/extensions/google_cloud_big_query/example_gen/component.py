febdc6e5ae876499e7a736efd19ed36aec089fd0
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2024-01-03 02:47:23 -0800

    Allow placeholder into BigQueryExampleGen(range_config)
    PiperOrigin-RevId: 595344954

--- tfx/extensions/google_cloud_big_query/example_gen/component.py
+++ tfx/extensions/google_cloud_big_query/example_gen/component.py
@@ -18,6 +18,7 @@ from typing import Optional, Union
 from tfx.components.example_gen import component
 from tfx.components.example_gen import utils
 from tfx.dsl.components.base import executor_spec
+from tfx.dsl.placeholder import placeholder as ph
 from tfx.extensions.google_cloud_big_query.example_gen import executor
 from tfx.orchestration import data_types
 from tfx.proto import example_gen_pb2
@@ -47,7 +48,11 @@ class BigQueryExampleGen(component.QueryBasedExampleGen):
           Union[example_gen_pb2.Output, data_types.RuntimeParameter]
       ] = None,
       range_config: Optional[
-          Union[range_config_pb2.RangeConfig, data_types.RuntimeParameter]
+          Union[
+              range_config_pb2.RangeConfig,
+              data_types.RuntimeParameter,
+              ph.Placeholder,
+          ]
       ] = None,
       custom_executor_spec: Optional[executor_spec.ExecutorSpec] = None,
       custom_config: Optional[
