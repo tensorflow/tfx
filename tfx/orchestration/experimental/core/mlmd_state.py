c765bd3b10629cfc16ae486c613ad01c4f6b5b02
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2024-01-02 16:57:22 -0800

    Update put_execution to use field masks
    PiperOrigin-RevId: 595241882

--- tfx/orchestration/experimental/core/mlmd_state.py
+++ tfx/orchestration/experimental/core/mlmd_state.py
@@ -23,6 +23,7 @@ from typing import Callable, Iterator, MutableMapping, Optional
 import cachetools
 from tfx.orchestration import metadata
 
+from google.protobuf.internal import containers
 from ml_metadata.proto import metadata_store_pb2
 
 
@@ -84,9 +85,10 @@ class _ExecutionCache:
     return execution
 
   def put_execution(self, mlmd_handle: metadata.Metadata,
-                    execution: metadata_store_pb2.Execution) -> None:
+                    execution: metadata_store_pb2.Execution,
+                    field_mask_paths: Optional[list[str]] = None) -> None:
     """Writes execution to MLMD and updates cache."""
-    mlmd_handle.store.put_executions([execution])
+    mlmd_handle.store.put_executions([execution], field_mask_paths)
     # The execution is fetched from MLMD again to ensure that the in-memory
     # value of `last_update_time_since_epoch` of the execution is same as the
     # one stored in MLMD.
@@ -150,11 +152,27 @@ def mlmd_execution_atomic_op(
     if execution != execution_copy:
       if execution.id != execution_copy.id:
         raise RuntimeError(
-            'Execution id should not be changed within mlmd_execution_atomic_op '
-            'context.')
+            'Execution id should not be changed within mlmd_execution_atomic_op'
+            ' context.')
+
+      # Orchestrator code will only update top-level fields and properties/
+      # custom properties with diffs.
+
+      # Motivation: to allow non-orchestrator code (specifically, pipeline tags
+      # and labels) to modify execution custom properties while the orchestrator
+      # is running. Delta changes are only applied for masked properties /
+      # custom properties. execution.last_known_state will always be updated.
+
+      # It enables orchestrator and non-orchestrator codes to run concurrently
+      # as long as there are no overlaps in the modified fields.
+
       # Make a copy before writing to cache as the yielded `execution_copy`
       # object may be modified even after exiting the contextmanager.
-      _execution_cache.put_execution(mlmd_handle, copy.deepcopy(execution_copy))
+      _execution_cache.put_execution(
+          mlmd_handle,
+          copy.deepcopy(execution_copy),
+          get_field_mask_paths(execution, execution_copy),
+      )
       if on_commit is not None:
         pre_commit_execution = copy.deepcopy(execution)
         post_commit_execution = copy.deepcopy(
@@ -185,3 +203,58 @@ def evict_from_cache(execution_id: int) -> Iterator[None]:
 def clear_in_memory_state():
   """Clears cached state. Useful in tests."""
   _execution_cache.clear_cache()
+
+
+def get_field_mask_paths(
+    execution: metadata_store_pb2.Execution,
+    execution_copy: metadata_store_pb2.Execution,
+) -> list[str]:
+  """Get Execution field mask paths for mutations.
+
+  Args:
+    execution: original in-memory state of an MLMD execution.
+    execution_copy: in-memory state of an MLMD execution after mutations.
+
+  Returns:
+    All top-level field paths, and property / custom property fields with diffs.
+    Only field paths in the mask will be updated during MLMD commits.
+  """
+  field_mask_paths = []
+
+  # Get all non-property field paths.
+  for field in metadata_store_pb2.Execution.DESCRIPTOR.fields:
+    # Skip property fields.
+    if field.name not in ['properties', 'custom_properties']:
+      field_mask_paths.append(field.name)
+
+  # Get property names with diffs.  Note that Python supports == operator for
+  # proto messages.
+  def _get_property_names_with_diff(
+      properties: containers.MessageMap[str, metadata_store_pb2.Value],
+      copy_properties: containers.MessageMap[str, metadata_store_pb2.Value],
+  ) -> list[str]:
+    property_names_with_diff = []
+    for name in set(properties.keys()).union(set(copy_properties.keys())):
+      if (
+          name in properties
+          and name in copy_properties
+          and properties[name] == copy_properties[name]
+      ):
+        continue
+      property_names_with_diff.append(name)
+    return property_names_with_diff
+
+  property_names_with_diff = _get_property_names_with_diff(
+      execution.properties, execution_copy.properties
+  )
+  custom_property_names_with_diff = _get_property_names_with_diff(
+      execution.custom_properties, execution_copy.custom_properties
+  )
+
+  field_mask_paths.extend(
+      [f'properties.{name}' for name in property_names_with_diff]
+  )
+  field_mask_paths.extend(
+      [f'custom_properties.{name}' for name in custom_property_names_with_diff]
+  )
+  return field_mask_paths
