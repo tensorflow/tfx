050a41b7fc4574a9eaed27020afff7a0f19253a3
anuartb <anuartb@google.com>
2024-04-03 21:34:21 -0700

    no-op
    PiperOrigin-RevId: 621727292

--- tfx/dependencies.py
+++ tfx/dependencies.py
@@ -90,6 +90,9 @@ def make_required_install_packages():
       'kubernetes>=10.0.1,<13',
       'numpy>=1.16,<2',
       'pyarrow>=10,<11',
+      # TODO(b/332616741): Scipy version 1.13 breaks the TFX OSS test.
+      # Unpin once the issue is resolved.
+      'scipy<1.13',
       # TODO(b/291837844): Pinned pyyaml to 5.3.1.
       # Unpin once the issue with installation is resolved.
       'pyyaml>=6,<7',
