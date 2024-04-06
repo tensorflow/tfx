f0c8140d2b8b688939fae9c640e3c7c6f7f5d019
wssong <wssong@google.com>
2024-04-06 08:45:18 -0700

    Update tensorflow-serving-api version constraint and update CAIP Prediction compatibility table to use TF 2.15
    PiperOrigin-RevId: 622452080

--- tfx/dependencies.py
+++ tfx/dependencies.py
@@ -117,7 +117,7 @@ def make_required_install_packages():
           nightly='>=0.46.0.dev',
           git_master='@git+https://github.com/tensorflow/model-analysis@master',
       ),
-      'tensorflow-serving-api>=1.15,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*,!=2.8.*,<3',
+      'tensorflow-serving-api>=2.15,<2.16',
       'tensorflow-transform'
       + select_constraint(
           default='>=1.14.0,<1.15.0',
