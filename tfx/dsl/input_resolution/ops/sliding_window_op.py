870e70a13a41a10772e70393b6a9e7d9fb1e6cee
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2024-06-10 18:23:33 -0700

    add stride parameter into SlideWindow Class.
    PiperOrigin-RevId: 642088461

--- tfx/dsl/input_resolution/ops/sliding_window_op.py
+++ tfx/dsl/input_resolution/ops/sliding_window_op.py
@@ -29,21 +29,38 @@ class SlidingWindow(
   # The length of the sliding window, must be > 0.
   window_size = resolver_op.Property(type=int, default=1)
 
+  # The stride of the sliding window, must be > 0.
+  stride = resolver_op.Property(type=int, default=1)
+
   # The output key for the dicts in the returned ARTIFACT_MULTIMAP_LIST.
   output_key = resolver_op.Property(type=str, default='window')
 
   def apply(
       self, input_list: Sequence[types.Artifact]
   ) -> Sequence[Mapping[str, Sequence[types.Artifact]]]:
-    """Applies a sliding window of size n to the list of artifacts.
+    """Applies a sliding window of size n and stride m to the list of artifacts.
 
-    For example, for artifacts [A, B, C, D] with n=2, then a sliding window of 2
-    will be applied, producing [[A, B], [B, C], [C, D]]. The stride is set to 1
-    by default.
+    Examples:
 
-    Note that what will actually be returned is a an ARTIFACT_MULTIMAP_LIST:
-    [{"window": [A, B]}, {"window": [B, C]}, {"window": [C, D]}]. The output_key
-    is set to "window" by default.
+    a)For artifacts [A, B, C, D] with window_size=2, stride=1,
+    produces [[A, B],[B, C], [C, D]].
+
+    b)For artifacts [A, B, C, D] with window_size=2, stride=2,
+    produces [[A, B], [C, D]].
+
+    c)For artifacts [A, B, C, D] with window_size=2, stride=3,
+    produces [[A, B]].
+
+    d)For artifacts [A, B, C] with window_size=2, stride=2,
+    produces [[A, B]].
+
+    Note that artifacts at the end of input_list that do not fit into a full
+    window of size n will be discarded. We do not support padding for now.
+
+    This function will actually return an
+    ARTIFACT_MULTIMAP_LIST:
+      [{"window": [A, B]}, {"window": [B, C]}, {"window": [C, D]}].
+    The output_key is set to "window" by default.
 
     This is because a type of ARTIFACT_LIST_LIST is not yet supported in the IR
     compilation. The dictionaries will have to be unnested in the resolver
@@ -58,11 +75,20 @@ class SlidingWindow(
     """
     if self.window_size < 1:
       raise ValueError(
-          f'sliding_window must be > 0, but was set to {self.window_size}.')
+          f'window_size must be > 0 , but was set to {self.window_size}.'
+      )
+
+    if self.stride < 1:
+      raise ValueError(
+          f'stride must be > 0, but was set to {self.stride}.'
+      )
 
     if not input_list:
       return []
 
-    num_windows = len(input_list) - self.window_size + 1
-    windows = [input_list[i:(i + self.window_size)] for i in range(num_windows)]
+    windows = [
+        input_list[i : i + self.window_size]
+        for i in range(0, len(input_list) - self.window_size + 1, self.stride)
+    ]
+
     return [{self.output_key: window} for window in windows]
