76d5946d6f1760dbcb0c470c4718796e52b9d487
Doojin Park <doojin@google.com>
2024-10-21 04:58:10 +0000

    keep config object to prevent any unintended issues

--- tfx/conftest.py
+++ tfx/conftest.py
@@ -2,7 +2,6 @@
 from absl import flags
 
 def pytest_configure(config):
-  del config
   # This is needed to avoid
   # `absl.flags._exceptions.UnparsedFlagAccessError` in some tests.
   flags.FLAGS.mark_as_parsed()
