48eafcab55e91f917faf8d237360d38eda13e8a3
Doojin Park <doojin@google.com>
2024-10-23 06:27:42 +0000

    Re-enable more xfail test cases

--- tfx/orchestration/portable/input_resolution/channel_resolver_test.py
+++ tfx/orchestration/portable/input_resolution/channel_resolver_test.py
@@ -14,7 +14,6 @@
 """Tests for tfx.orchestration.portable.input_resolution.channel_resolver."""
 
 
-import pytest
 from tfx.orchestration.portable.input_resolution import channel_resolver
 from tfx.proto.orchestration import pipeline_pb2
 from tfx.utils import test_case_utils
@@ -109,8 +108,6 @@ class ChannelResolverTest(test_case_utils.TfxTest, test_case_utils.MlmdMixins):
           self.mlmd_handle, ch)
       self.assertEmpty(resolved)
 
-  @pytest.mark.xfail(run=False, reason="PR 6889 This test fails and needs to be fixed. "
-"If this test passes, please remove this mark.")
   def testResolveSingleChannel_AllContexts(self):
     p = self.put_context('pipeline', 'my-pipeline')
     r1 = self.put_context('pipeline_run', 'run-001')
@@ -227,8 +224,6 @@ class ChannelResolverTest(test_case_utils.TfxTest, test_case_utils.MlmdMixins):
           self.mlmd_handle, ch)
       self.assertEmpty(resolved)
 
-  @pytest.mark.xfail(run=False, reason="PR 6889 This test fails and needs to be fixed. "
-"If this test passes, please remove this mark.")
   def testResolveSingleChannel_OutputKey(self):
     p = self.put_context('pipeline', 'my-pipeline')
     e1 = self.put_artifact('Examples')
@@ -308,8 +303,6 @@ class ChannelResolverTest(test_case_utils.TfxTest, test_case_utils.MlmdMixins):
           self.mlmd_handle, ch)
       self.assertEqual({a.id for a in resolved}, {e1.id, e2.id})
 
-  @pytest.mark.xfail(run=False, reason="PR 6889 This test fails and needs to be fixed. "
-"If this test passes, please remove this mark.")
   def testResolveSingleChannel_BadArtifactQuery(self):
     p = self.put_context('pipeline', 'my-pipeline')
     self.put_execution(
@@ -427,8 +420,6 @@ class ChannelResolverTest(test_case_utils.TfxTest, test_case_utils.MlmdMixins):
         self.mlmd_handle, ch)
     self.assertEmpty(resolved)
 
-  @pytest.mark.xfail(run=False, reason="PR 6889 This test fails and needs to be fixed. "
-"If this test passes, please remove this mark.", strict=True)
   def testResolveUnionChannels_Deduplication(self):
     p = self.put_context('pipeline', 'my-pipeline')
     e1 = self.put_artifact('Examples')
