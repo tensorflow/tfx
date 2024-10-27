2a26c9eab6be477cb7218c27a4f0ee65e4e05953
Doojin Park <doojin@google.com>
2024-10-27 12:55:41 +0000

    keep xFail for the some e2e tests

--- tfx/examples/tfjs_next_page_prediction/tfjs_next_page_prediction_e2e_test.py
+++ tfx/examples/tfjs_next_page_prediction/tfjs_next_page_prediction_e2e_test.py
@@ -31,6 +31,8 @@ except ImportError:
 import pytest
 
 
+@pytest.mark.xfail(run=False, reason="PR 6889 This class contains tests that fail and needs to be fixed. "
+"If all tests pass, please remove this mark.")
 @pytest.mark.e2e
 @unittest.skipIf(tensorflowjs is None,
                  'Cannot import required modules. This can happen when'
