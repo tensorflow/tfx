38b165713e3544d56de7919e4ecfcfd524ad32b5
Doojin Park <doojin@google.com>
2024-12-02 05:04:59 +0000

    Update example template models not to use deprecated Keras APIs

--- tfx/experimental/templates/taxi/models/keras_model/model.py
+++ tfx/experimental/templates/taxi/models/keras_model/model.py
@@ -136,15 +136,9 @@ def _build_keras_model(hidden_units, learning_rate):
     deep = tf.keras.layers.Dense(numnodes)(deep)
 
   wide_layers = []
-  for key in _transformed_names(_VOCAB_FEATURE_KEYS):
+  for key in features.transformed_names(features.VOCAB_FEATURE_KEYS):
     wide_layers.append(
-        tf.keras.layers.CategoryEncoding(num_tokens=_VOCAB_SIZE + _OOV_SIZE)(
-            input_layers[key]
-        )
-    )
-  for key in _transformed_names(_BUCKET_FEATURE_KEYS):
-    wide_layers.append(
-        tf.keras.layers.CategoryEncoding(num_tokens=_FEATURE_BUCKET_COUNT)(
+        tf.keras.layers.CategoryEncoding(num_tokens=features.VOCAB_SIZE + features.OOV_SIZE)(
             input_layers[key]
         )
     )
