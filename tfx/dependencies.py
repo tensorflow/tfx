0d40134f066290c3fac75ecb70461512bcd5e3af
Venkata Sai Madhur Karampudi <vkarampudi@google.com>
2026-02-13 02:52:22 +0000

    feat: Upgrade kfp and its dependencies to 2.6.0

--- tfx/dependencies.py
+++ tfx/dependencies.py
@@ -146,8 +146,8 @@ def make_extra_packages_airflow():
 def make_extra_packages_kfp():
   """Prepare extra packages needed for Kubeflow Pipelines orchestrator."""
   return [
-      "kfp>=2.15.2",
-      "kfp-pipeline-spec>=2.15.2",
+      "kfp>=2.6.0",
+      "kfp-pipeline-spec>=2.6.0",
   ]
 
 
@@ -168,8 +168,8 @@ def make_extra_packages_test():
 def make_extra_packages_docker_image():
     # Packages needed for tfx docker image.
     return [
-        "kfp>=2",
-        "kfp-pipeline-spec>=0.2.2",
+        "kfp>=2.6.0",
+        "kfp-pipeline-spec>=2.6.0",
         "mmh>=2.2,<3",
         "python-snappy>=0.7",
         # Required for tfx/examples/penguin/penguin_utils_cloud_tuner.py
