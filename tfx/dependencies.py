306af280ebcfe01d008076d8d00e51d980d2648d
Venkata Sai Madhur Karampudi <vkarampudi@google.com>
2026-02-13 05:07:45 +0000

    fix: Align kfp versions to 2.15.2

--- tfx/dependencies.py
+++ tfx/dependencies.py
@@ -146,8 +146,8 @@ def make_extra_packages_airflow():
 def make_extra_packages_kfp():
   """Prepare extra packages needed for Kubeflow Pipelines orchestrator."""
   return [
-      "kfp>=2.6.0",
-      "kfp-pipeline-spec>=2.6.0",
+      "kfp>=2.15.2",
+      "kfp-pipeline-spec>=2.15.2",
   ]
 
 
@@ -168,8 +168,8 @@ def make_extra_packages_test():
 def make_extra_packages_docker_image():
     # Packages needed for tfx docker image.
     return [
-        "kfp>=2.6.0",
-        "kfp-pipeline-spec>=2.6.0",
+        "kfp>=2.15.2",
+        "kfp-pipeline-spec>=2.15.2",
         "mmh>=2.2,<3",
         "python-snappy>=0.7",
         # Required for tfx/examples/penguin/penguin_utils_cloud_tuner.py
