4e2d8ca5cf72a7f722c08c560153a6138713a09f
Venkata Sai Madhur Karampudi <vkarampudi@google.com>
2026-05-19 18:09:32 +0000

    Disable Protobuf runtime version validation check by default in TFX entrypoint

--- tfx/__init__.py
+++ tfx/__init__.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 """Init module for TFX."""
 
+import os
+os.environ['TEMPORARILY_DISABLE_PROTOBUF_VERSION_CHECK'] = 'true'
+
 # `tfx` is a namespace package.
 # https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
