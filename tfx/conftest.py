b9130b04cc68b89a7e3cd463f0b0b850bf069989
Venkata Sai Madhur Karampudi <vkarampudi@google.com>
2026-05-19 18:55:26 +0000

    Exclude optional Kubeflow and ranking tests dynamically from pytest collection when dependencies are absent

--- tfx/conftest.py
+++ tfx/conftest.py
@@ -5,3 +5,21 @@ def pytest_configure(config):
   # This is needed to avoid
   # `absl.flags._exceptions.UnparsedFlagAccessError` in some tests.
   flags.FLAGS.mark_as_parsed()
+
+
+def pytest_ignore_collect(collection_path, config):
+  path_str = str(collection_path)
+  # Ignore Kubeflow related tests if kfp is not installed
+  if 'kubeflow' in path_str or 'kfp' in path_str:
+    try:
+      import kfp  # noqa: F401
+    except ImportError:
+      return True
+  # Ignore ranking tests if struct2tensor is not installed/functional
+  if 'ranking' in path_str:
+    try:
+      import struct2tensor  # noqa: F401
+    except Exception:
+      return True
+  return False
+
