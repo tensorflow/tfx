dd4d5ab682dad9523b28bfc7ccb3d60b63fdcdd6
Venkata Sai Madhur Karampudi <vkarampudi@google.com>
2026-05-19 20:35:16 +0000

    Dynamically exclude optional Airflow, Vertex and interactive tests from pytest collection when dependencies are not installed

--- tfx/conftest.py
+++ tfx/conftest.py
@@ -9,8 +9,8 @@ def pytest_configure(config):
 
 def pytest_ignore_collect(collection_path, config):
   path_str = str(collection_path)
-  # Ignore Kubeflow related tests if kfp is not installed
-  if 'kubeflow' in path_str or 'kfp' in path_str:
+  # Ignore Kubeflow/Vertex related tests if kfp is not installed
+  if any(k in path_str for k in ('kubeflow', 'kfp', 'vertex')):
     try:
       import kfp  # noqa: F401
     except ImportError:
@@ -21,4 +21,17 @@ def pytest_ignore_collect(collection_path, config):
       import struct2tensor  # noqa: F401
     except Exception:
       return True
+  # Ignore Airflow related tests if airflow is not installed
+  if 'airflow' in path_str or 'chicago_taxi_pipeline/taxi_pipeline_simple_test' in path_str:
+    try:
+      import airflow  # noqa: F401
+    except ImportError:
+      return True
+  # Ignore interactive context tests if nbformat is not installed
+  if 'interactive_context' in path_str:
+    try:
+      import nbformat  # noqa: F401
+    except ImportError:
+      return True
   return False
+
